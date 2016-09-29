f59d4d1120cf9743245d9df8aadf16b900cf5832
Michael Deakin <mdeakin@sandia.gov>
2016-09-29 14:07:00 -0600

    Add cmake test files for swtc*_flat

--- test/reg_test/run_tests/test-list.cmake
+++ test/reg_test/run_tests/test-list.cmake
@@ -4,6 +4,10 @@ SET(HOMME_TESTS
   swtc2.cmake
   swtc5.cmake
   swtc6.cmake
+  swtc1_flat.cmake
+  swtc2_flat.cmake
+  swtc5_flat.cmake
+  swtc6_flat.cmake
   swimtc5.cmake
   baro2b.cmake
   baro2c.cmake
