e10729d9e4709b0e9472b667819cbb6cfe5991e3
Michael Deakin <mdeakin@sandia.gov>
2016-10-13 09:58:42 -0600

    Add a check for Trilinos; if it's not found build and install it; otherwise use the version installed at the prefix specified by TRILINOS_INSTALL_DIR

--- cmake/Trilinos.cmake
+++ cmake/Trilinos.cmake
@@ -15,18 +15,51 @@ ENDIF()
 
 SET(TRILINOS_INSTALL_DIR "~/prefix" CACHE FILEPATH "Where to install Trilinos")
 
-SET(TRILINOS_CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${TRILINOS_INSTALL_DIR} -DTrilinos_ENABLE_Kokkos=ON ${EXECUTION_SPACES})
+FIND_PACKAGE(Trilinos QUIET PATHS ${TRILINOS_INSTALL_DIR}/lib/cmake/Trilinos)
 
-INCLUDE(ExternalProject)
+IF(NOT Trilinos_FOUND)
+  SET(TRILINOS_CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${TRILINOS_INSTALL_DIR} -DTrilinos_ENABLE_Kokkos=ON ${EXECUTION_SPACES})
 
-EXTERNALPROJECT_ADD(Trilinos
+  INCLUDE(ExternalProject)
 
-  GIT_REPOSITORY ${TRILINOS_REPO}
-  GIT_TAG master
-  UPDATE_COMMAND ""
-  PATCH_COMMAND ""
+  EXTERNALPROJECT_ADD(Trilinos
 
-  SOURCE_DIR ${TRILINOS_SRCDIR}
+    GIT_REPOSITORY ${TRILINOS_REPO}
+    GIT_TAG master
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
 
-  CMAKE_ARGS ${TRILINOS_CMAKE_ARGS}
-)
+    SOURCE_DIR ${TRILINOS_SRCDIR}
+
+    CMAKE_ARGS ${TRILINOS_CMAKE_ARGS}
+  )
+  FIND_PACKAGE(Trilinos REQUIRED PATHS ${TRILINOS_INSTALL_DIR}/lib/cmake/Trilinos)
+ENDIF()
+
+IF("${Trilinos_PACKAGE_LIST}" MATCHES "Kokkos")
+  MESSAGE("Kokkos found!")
+ELSE()
+  MESSAGE(FATAL_ERROR "Kokkos not found in Trilinos install; aborting")
+ENDIF()
+
+MESSAGE("\nFound Trilinos!  Here are the details: ")
+MESSAGE("   Trilinos_DIR = ${Trilinos_DIR}")
+MESSAGE("   Trilinos_VERSION = ${Trilinos_VERSION}")
+MESSAGE("   Trilinos_PACKAGE_LIST = ${Trilinos_PACKAGE_LIST}")
+MESSAGE("   Trilinos_LIBRARIES = ${Trilinos_LIBRARIES}")
+MESSAGE("   Trilinos_INCLUDE_DIRS = ${Trilinos_INCLUDE_DIRS}")
+MESSAGE("   Trilinos_TPL_LIST = ${Trilinos_TPL_LIST}")
+MESSAGE("   Trilinos_TPL_INCLUDE_DIRS = ${Trilinos_TPL_INCLUDE_DIRS}")
+MESSAGE("   Trilinos_TPL_LIBRARIES = ${Trilinos_TPL_LIBRARIES}")
+MESSAGE("   Trilinos_BUILD_SHARED_LIBS = ${Trilinos_BUILD_SHARED_LIBS}")
+MESSAGE("   Trilinos_CXX_COMPILER = ${Trilinos_CXX_COMPILER}")
+MESSAGE("   Trilinos_C_COMPILER = ${Trilinos_C_COMPILER}")
+MESSAGE("   Trilinos_Fortran_COMPILER = ${Trilinos_Fortran_COMPILER}")
+MESSAGE("   Trilinos_CXX_COMPILER_FLAGS = ${Trilinos_CXX_COMPILER_FLAGS}")
+MESSAGE("   Trilinos_C_COMPILER_FLAGS = ${Trilinos_C_COMPILER_FLAGS}")
+MESSAGE("   Trilinos_Fortran_COMPILER_FLAGS = ${Trilinos_Fortran_COMPILER_FLAGS}")
+MESSAGE("   Trilinos_LINKER = ${Trilinos_LINKER}")
+MESSAGE("   Trilinos_EXTRA_LD_FLAGS = ${Trilinos_EXTRA_LD_FLAGS}")
+MESSAGE("   Trilinos_AR = ${Trilinos_AR}")
+MESSAGE("End of Trilinos details\n")
+
