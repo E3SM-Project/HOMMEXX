853d751008c30e97ff49de224cbbad4f66f1fa6b
Michael Deakin <mdeakin@sandia.gov>
2016-10-17 16:21:34 -0600

    Starting work on linking C++ and Fortran

--- components/homme/src/sweqx_flat/unit_tests/divergence.cpp
+++ components/homme/src/sweqx_flat/unit_tests/divergence.cpp
@@ -1,5 +1,12 @@
 
 #include <catch/catch.hpp>
 
+#include <fortran_binding.hpp>
+
+void divergence_sphere(const void *, const void *, const void *, void *);
+
+extern void f90_divergence_sphere(const void *, const void *, const void *, void *) FORTRAN(divergence_sphere);
+
 TEST_CASE("Divergence Test Case", "divergence_cxx") {
+  f90_divergence_sphere(NULL, NULL, NULL, NULL);
 }
