9d9cffdb1093b3af521731157644a6c446b0e703
Michael Deakin <mdeakin@sandia.gov>
2016-10-25 17:23:05 -0600

    Add kokkos_init.cpp

---
+++ components/homme/src/share/kokkos_init.cpp
@@ -0,0 +1,17 @@
+
+#include <Kokkos_Core.hpp>
+
+namespace Homme {
+
+extern "C" {
+void init_kokkos() {
+  /* Set OpenMP Environment variables to control how many
+   * threads/processors Kokkos uses */
+  Kokkos::initialize();
+  Kokkos::OpenMP::print_configuration(std::cout, true);
+}
+
+void finalize_kokkos() { Kokkos::finalize(); }
+}
+}  // namespace Homme
+
