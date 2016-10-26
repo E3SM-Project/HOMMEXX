adcbafe628ba45ad7cb77f2c8ee2eab4279a829a
Michael Deakin <mdeakin@sandia.gov>
2016-10-26 10:57:25 -0600

    Fix Kokkos version of recover_q to recieve addresses of parameters rather than actual addresses (need to check p_ptr)

--- src/share/kokkos_init.cpp
+++ src/share/kokkos_init.cpp
@@ -1,6 +1,8 @@
 
 #include <Kokkos_Core.hpp>
 
+#include <iostream>
+
 namespace Homme {
 
 extern "C" {
@@ -9,6 +11,11 @@ void init_kokkos() {
    * threads/processors Kokkos uses */
   Kokkos::initialize();
   Kokkos::OpenMP::print_configuration(std::cout, true);
+
+  std::cout << "Kokkos initialized!" << std::endl <<
+    "omp_max_threads: " << omp_get_max_threads() <<
+    std::endl << "omp_get_num_threads: " <<
+    omp_get_num_threads() << std::endl;
 }
 
 void finalize_kokkos() { Kokkos::finalize(); }
