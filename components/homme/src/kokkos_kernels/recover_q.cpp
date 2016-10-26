48c727a865da4180a30b22dcfc858a4e3a969c49
Michael Deakin <mdeakin@sandia.gov>
2016-10-26 10:57:25 -0600

    Fix Kokkos version of recover_q to recieve addresses of parameters rather than actual addresses (need to check p_ptr)

--- components/homme/src/kokkos_kernels/recover_q.cpp
+++ components/homme/src/kokkos_kernels/recover_q.cpp
@@ -5,6 +5,7 @@
 #include <kinds.hpp>
 
 #include <iostream>
+#include <stdexcept>
 
 namespace Homme {
 
@@ -19,8 +20,9 @@ constexpr const int TIMELEVELS = 3;
 
 extern "C" {
 #if 0
-void recover_q(int &nets, int &nete, int &kmass, int nelems, int &n0,
-               double *p) {
+void recover_q(const int &nets, const int &nete,
+               const int &kmass, const int &nelems,
+               const int &n0, double *p) noexcept {
   if(kmass != -1) {
     for(int ie = nets - 1; ie < nete; ++ie) {
       for(int k = 0; k < nlev; ++k) {
@@ -39,29 +41,36 @@ void recover_q(int &nets, int &nete, int &kmass, int nelems, int &n0,
 
 #else
 
-void recover_q(int nets, int nete, int kmass, int n0,
-               int nelems, double *p_ptr) {
+void recover_q(const int &nets, const int &nete,
+               const int &kmass, const int &n0,
+               const int &nelems, double *p_ptr) noexcept {
   using RangePolicy = Kokkos::Experimental::MDRangePolicy<
       Kokkos::Experimental::Rank<
           2, Kokkos::Experimental::Iterate::Left,
           Kokkos::Experimental::Iterate::Left>,
       Kokkos::IndexType<int> >;
-  P p(p_ptr, np, np, nlev, TIMELEVELS,
-      nelems);  // unmanaged view - FIXME: implement this
-                // typedef (Dan)
+  P p(p_ptr, np, np, nlev, TIMELEVELS, nelems);
   if(kmass != -1) {
-    Kokkos::Experimental::md_parallel_for(
-        RangePolicy({0, nets - 1}, {nlev, nete}, {1, 1}),
-        KOKKOS_LAMBDA(int k, int ie) {
-          if(k != kmass) {
-            for(int j = 0; j < np; ++j) {
-              for(int i = 0; i < np; ++i) {
-                p(i, j, k, n0, ie) /=
-                    p(i, j, kmass, n0, ie);
+    try {
+      Kokkos::Experimental::md_parallel_for(
+          RangePolicy({0, nets - 1}, {nlev, nete}, {1, 1}),
+          KOKKOS_LAMBDA(int k, int ie) {
+            if(k != kmass) {
+              for(int j = 0; j < np; ++j) {
+                for(int i = 0; i < np; ++i) {
+                  p(i, j, k, n0, ie) /=
+                      p(i, j, kmass, n0, ie);
+                }
               }
             }
-          }
-        });
+          });
+    } catch(std::exception &e) {
+      std::cout << e.what() << std::endl;
+      std::abort();
+    } catch(...) {
+      std::cout << "Unknown exception" << std::endl;
+      std::abort();
+    }
   }
 }
 #endif
