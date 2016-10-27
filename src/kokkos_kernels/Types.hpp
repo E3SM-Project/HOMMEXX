89bb84565891f4eb33cfcc99b7b01cddbb132616
Michael Deakin <mdeakin@sandia.gov>
2016-10-26 19:51:42 -0600

    Implement loop3 in C++, with both Kokkos and vanilla C

--- src/kokkos_kernels/Types.hpp
+++ src/kokkos_kernels/Types.hpp
@@ -5,17 +5,8 @@
 
 namespace Homme {
 
-  //using TeamMember  = Kokkos::TeamPolicy< Kokkos::IndexType<int> >::member_type;
-  //using SharedSpace = Kokkos::DefaultExecutionSpace::scratch_memory_space;
-
-using Dinv   = Kokkos::View<real*****,  Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-  using P      = Kokkos::View<real*****,  Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-using PS     = Kokkos::View<real***,    Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-using Deriv  = Kokkos::View<real***,    Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-using GradPS = Kokkos::View<real****,   Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-using V      = Kokkos::View<real******, Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-using Det    = Kokkos::View<real***,    Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
-
-
+using D = Kokkos::View<real*****,  Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
+using P = Kokkos::View<real*****,  Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
+using V = Kokkos::View<real******, Kokkos::LayoutLeft, Kokkos::MemoryUnmanaged>;
 
 } // namespace Homme
