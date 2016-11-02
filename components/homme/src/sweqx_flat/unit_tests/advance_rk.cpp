03beaabc8b25e3efc2122bef07912830ab47ec9a
Michael Deakin <mdeakin@sandia.gov>
2016-11-02 10:30:33 -0600

    Add test case for loop6

--- components/homme/src/sweqx_flat/unit_tests/advance_rk.cpp
+++ components/homme/src/sweqx_flat/unit_tests/advance_rk.cpp
@@ -155,7 +155,58 @@ TEST_CASE("q_tests", "advance_nonstag_rk_cxx") {
   delete[] p_theory;
 }
 
-TEST_CASE("loop3", "advance_nonstag_rk_cxx") {
+TEST_CASE("loop6", "advance_nonstag_rk_cxx") {
+  constexpr const int numelems = 100;
+
+  // real elem_state_p (np,np,nlevel,timelevels,nelemd)
+  constexpr const int p_len =
+      numelems * timelevels * nlev * np * np;
+  real *p_theory = new real[p_len];
+  real *p_exper = new real[p_len];
+
+  constexpr const int numRandTests = 10;
+
+  SECTION("random test") {
+    std::random_device rd;
+    using rngAlg = std::mt19937_64;
+    rngAlg engine(rd());
+    for(int i = 0; i < numRandTests; i++) {
+      const int nets = (std::uniform_int_distribution<int>(
+          1, numelems - 1))(engine);
+      const int nete = (std::uniform_int_distribution<int>(
+          nets + 1, numelems))(engine);
+      const int n0 = (std::uniform_int_distribution<int>(
+          1, timelevels))(engine);
+      int kmass = (std::uniform_int_distribution<int>(
+          0, nlev))(engine);
+      /* kmass needs to be in [1, nlev] or be -1 for the
+       * Fortran implementation */
+      if(kmass == 0) {
+        kmass = -1;
+      }
+
+      std::uniform_real_distribution<real> p_dist(0, 1.0);
+      for(int j = 0; j < p_len; j++) {
+        p_theory[j] = p_dist(engine);
+        p_exper[j] = p_theory[j];
+      }
+      loop6_f90(nets, nete, kmass, n0, numelems, p_theory);
+      loop6_c(nets, nete, kmass, n0, numelems, p_exper);
+      for(int j = 0; j < p_len; j++) {
+        REQUIRE(p_exper[j] == p_theory[j]);
+      }
+      loop6_f90(nets, nete, kmass, n0, numelems, p_theory);
+      loop6_c(nets, nete, kmass, n0, numelems, p_exper);
+      for(int j = 0; j < p_len; j++) {
+        REQUIRE(p_exper[j] == p_theory[j]);
+      }
+    }
+  }
+  delete[] p_exper;
+  delete[] p_theory;
+}
+
+TEST_CASE("contra2latlon", "advance_nonstag_rk_cxx") {
   constexpr const int numelems = 100;
   constexpr const int dim = 2;
   // real elem_D (np,np,2,2,nelemd)
