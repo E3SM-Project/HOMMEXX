13725d7b4c54b323dbf72ab25c653cd0a9115eda
Michael Deakin <mdeakin@sandia.gov>
2016-11-04 16:05:44 -0600

    Initial work on implementing loop 7 in C++. No Kokkos version work yet

--- src/kokkos_kernels/Types.hpp
+++ src/kokkos_kernels/Types.hpp
@@ -5,33 +5,42 @@
 
 namespace Homme {
 
-using Alpha = Kokkos::View<real *, Kokkos::LayoutLeft,
-			   Kokkos::MemoryUnmanaged>;
-using D = Kokkos::View<real *****, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
-using P = Kokkos::View<real *****, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
-using V = Kokkos::View<real ******, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
+template <typename T>
+using HommeView = Kokkos::View<T, Kokkos::LayoutLeft,
+                               Kokkos::MemoryUnmanaged>;
 
-using SphereMP = Kokkos::View<real ***, Kokkos::LayoutLeft,
-                              Kokkos::MemoryUnmanaged>;
-using PTens = Kokkos::View<real ****, Kokkos::LayoutLeft,
-                           Kokkos::MemoryUnmanaged>;
-using VTens = Kokkos::View<real *****, Kokkos::LayoutLeft,
-                           Kokkos::MemoryUnmanaged>;
+using Alpha = HommeView<real *>;
+using D = HommeView<real *****>;
+using P = HommeView<real *****>;
+using PS = HommeView<real ***>;
+using V = HommeView<real ******>;
+using SphereMP = HommeView<real ***>;
+using FCor = HommeView<real ***>;
+using PTens = HommeView<real ****>;
+using VTens = HommeView<real *****>;
 
-using D_noie = Kokkos::View<real ****, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
-using P_noie = Kokkos::View<real ****, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
-using V_noie = Kokkos::View<real *****, Kokkos::LayoutLeft,
-                       Kokkos::MemoryUnmanaged>;
-using SphereMP_noie = Kokkos::View<real **, Kokkos::LayoutLeft,
-				   Kokkos::MemoryUnmanaged>;
-using PTens_noie = Kokkos::View<real ***, Kokkos::LayoutLeft,
-                           Kokkos::MemoryUnmanaged>;
-using VTens_noie = Kokkos::View<real ****, Kokkos::LayoutLeft,
-                           Kokkos::MemoryUnmanaged>;
+enum Spherical_Polar_e { Radius, Lat, Lon };
+
+struct Spherical_Polar {
+  real radius;
+  real lon;
+  real lat;
+};
+
+using SphereP = HommeView<Kokkos::Array<real, 3> >;
+
+using ULatLong = Kokkos::View<real ***, Kokkos::LayoutLeft>;
+using Energy = Kokkos::View<real **, Kokkos::LayoutLeft>;
+using PV = Kokkos::View<real **, Kokkos::LayoutLeft>;
+
+struct derivative_t {
+  real Dvv[np][np];
+  real Dvv_diag[np][np]
+  real Dvv_twt[np][np]
+  real Mvv_twt[np][np]
+  real Mfvm[np][nc+1)
+  real Cfvm[np][nc]
+  real legdg[np][np]
+};
 
 }  // namespace Homme
