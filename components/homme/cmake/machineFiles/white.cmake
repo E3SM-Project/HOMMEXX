2d4ab033dcd7a009fcfe7052ca35d062cbd06dff
Michael Deakin <mdeakin@sandia.gov>
2016-11-21 15:36:21 -0700

    Add C and Fortran compiler choice to the white and RIDE cmake files

--- components/homme/cmake/machineFiles/white.cmake
+++ components/homme/cmake/machineFiles/white.cmake
@@ -1,3 +1,5 @@
+# Note: CMAKE_CXX_COMPILER needs to be set to the path of nvcc_wrapper
+# nvcc_wrapper will choose either the Nvidia Cuda compiler or the OpenMP compiler depending on what's being compiled
 
 SET(NETCDF_DIR $ENV{NETCDF_ROOT} CACHE FILEPATH "")
 SET(PNETCDF_DIR $ENV{PNETCDF_ROOT} CACHE FILEPATH "")
@@ -5,8 +7,13 @@ SET(PNETCDF_DIR $ENV{PNETCDF_ROOT} CACHE FILEPATH "")
 SET(NETCDF_Fortran_DIR $ENV{NETCDFF_ROOT} CACHE FILEPATH "")
 SET(NETCDFF_DIR $ENV{NETCDFF_ROOT} CACHE FILEPATH "")
 
+# Hacky way of ensuring needed libraries are linked in the proper order
+SET(NetcdfF_LIBRARY $ENV{NETCDFF_ROOT}/lib/libnetcdff.a -lnetcdf -lhdf5_hl -lhdf5 -ldl -lz CACHE LIST "")
 SET(ZLIB_DIR $ENV{ZLIB_ROOT} CACHE FILEPATH "")
 
 SET(HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
 
 SET(USE_QUEUING FALSE CACHE BOOL "")
+
+SET(CMAKE_C_COMPILER "mpicc" CACHE STRING "")
+SET(CMAKE_Fortran_COMPILER "mpifort" CACHE STRING "")
