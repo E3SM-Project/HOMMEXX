cfbc86277173687921656f7ab63730eebee1af10
Michael Deakin <mdeakin@sandia.gov>
2016-11-22 11:41:46 -0700

    Change to use the single hyphen for expt-extended-lambdas flag Always build with CUDA support if CUDA is in the Trilinos TPL list and Kokkos isn't being built Add ENABLE_CUDA flag on White

--- components/homme/cmake/Trilinos.cmake
+++ components/homme/cmake/Trilinos.cmake
@@ -44,9 +44,8 @@ IF(NOT Trilinos_FOUND OR NOT "${Trilinos_PACKAGE_LIST}" MATCHES "Kokkos")
           -DKokkos_ENABLE_CUDA_UVM=ON
 	  -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_TOOLKIT_ROOT_DIR}
 	  -DCMAKE_CXX_COMPILER=${NVCC_WRAPPER})
-      SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --expt-extended-lambda")
-      MESSAGE("Adding --expt-extended-lambda to the C++ compiler flags")
-      MESSAGE("${CMAKE_CXX_FLAGS}")
+      SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -expt-extended-lambda")
+      MESSAGE("CUDA Enabled")
     ENDIF()
   ENDIF()
 
@@ -91,11 +90,9 @@ ELSE()
   MESSAGE("   Trilinos_AR = ${Trilinos_AR}")
   MESSAGE("End of Trilinos details\n")
 
-  IF(ENABLE_CUDA)
+  IF(";${Trilinos_TPL_LIST};" MATCHES ";CUDA;")
     MESSAGE("CUDA Enabled")
-    IF(";${Trilinos_TPL_LIST};" MATCHES ";CUDA;")
-      SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --expt-extended-lambda")
-    ENDIF()
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -expt-extended-lambda")
   ENDIF()
 ENDIF()
 
