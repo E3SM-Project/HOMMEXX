1d386d348a6e2260859a8be91adc692933770538
Luca Bertagna <lbertag@sandia.gov>
2016-11-28 09:45:42 -0700

    Minor change in Derivative.cpp
    Changed the way the for loop that initializes the derivative arrays. In
    particular, instead of having looping on the 2d array dimensions, we now
    loop on the 1d array length, and recover the 2d indices on the fly.

--- components/homme/src/share/Derivative.cpp
+++ components/homme/src/share/Derivative.cpp
@@ -17,21 +17,45 @@ void init_derivative_c (real* const& Dvv,     real* const& Dvv_diag, real* const
                         real* const& Mvv_twt, real* const& Mfvm,     real* const& Cfvm,
                         real* const& legdg)
 {
+  constexpr const int np2 = np*np;
+  for (int i=0; i<np2; ++i)
+  {
+    // Note: in homme, the pseudo-spectral differentiation matrix is stored 'transposed':
+    //       instead of D_ij = d/dx(phi_j)(x_i) it is D_ij = d/dx(phi_i)(x_j)
+    //       Since here we want to restore the 'classical' expression of D,
+    //       and fortran stores matrices 'transposed' compared to C, we simply copy
+    //       the array as it is to balance the two effects
+    const int igp = i / np;
+    const int jgp = i % np;
+
+    get_derivative_c()->Dvv[igp][jgp]       = Dvv[i];
+  }
+
+  for (int i=0; i<np2; ++i)
+  {
+    const int igp = i % np;
+    const int jgp = i / np;
+
+    get_derivative_c()->Dvv_diag[igp][jgp]  = Dvv_diag[i];
+    get_derivative_c()->Dvv_twt[igp][jgp]   = Dvv_twt[i];
+    get_derivative_c()->Mvv_twt[igp][jgp]   = Mvv_twt[i];
+    get_derivative_c()->legdg[igp][jgp]     = legdg[i];
+  }
+
+  constexpr const int npnc = np*nc;
+  for (int i=0; i<npnc; ++i)
+  {
+    const int igp = i % np;
+    const int jcp = i / np;
+
+    get_derivative_c()->Mfvm[igp][jcp] = Mfvm[i];
+    get_derivative_c()->Cfvm[igp][jcp] = Cfvm[i];
+
+  }
+
   for (int igp=0; igp<np; ++igp)
   {
-    for (int jgp=0; jgp<np; ++jgp)
-    {
-      get_derivative_c()->Dvv[igp][jgp]       = Dvv[jgp*np+igp];
-      get_derivative_c()->Dvv_diag[igp][jgp]  = Dvv_diag[jgp*np+igp];
-      get_derivative_c()->Dvv_twt[igp][jgp]   = Dvv_twt[jgp*np+igp];
-      get_derivative_c()->legdg[igp][jgp]     = legdg[jgp*np+igp];
-    }
-    for (int jcp=0; jcp<nc; ++jcp)
-    {
-      get_derivative_c()->Mfvm[igp][jcp] = Mfvm[jcp*np+igp];
-      get_derivative_c()->Cfvm[igp][jcp] = Cfvm[jcp*np+igp];
-    }
-    get_derivative_c()->Mfvm[igp][nc-1] = Mfvm[(nc-1)*np+igp];
+    get_derivative_c()->Mfvm[igp][nc] = Mfvm[nc*np+igp];
   }
 }
 
