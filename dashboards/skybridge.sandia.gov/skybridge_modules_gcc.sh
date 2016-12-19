a4de4a5983921bbc7d4b6aa476db7b92e21198a6
Irina K. Tezaur <ikalash@sandia.gov>
2016-12-19 16:16:59 -0700

    Updating modules for skybridge to use intel/17.0 instead of intel/14.0. The lower version of the intel compiler was causing test failures.

--- dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
+++ dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
@@ -1,14 +1,11 @@
 source /projects/sems/modulefiles/utils/sems-modules-init.sh
 module load sems-env 
 module load sierra-git/2.6.1 
-module load sierra-compiler/intel/14.0 
-module load sierra-devel/intel-14.0-openmpi-1.6 
+module load sierra-compiler/intel/17.0 
+module load sierra-devel/intel-17.0-openmpi-1.6 
 module load sntools/master 
-module load intel/intel-14.0.4.211 
 module load openmpi-intel/1.6 
-module load sems-hdf5/1.8.12/parallel 
 module load sierra-python/2.7.4 
-module load sierra-mkl/14.0 
+module load sierra-mkl/17.0 
 module load sierra-mpi/openmpi/1.6
-module load sems-netcdf/4.3.2/parallel
 module list 
