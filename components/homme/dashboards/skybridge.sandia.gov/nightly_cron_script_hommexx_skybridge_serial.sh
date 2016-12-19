ae7e147a2ffb01951c2844459541f0572c43bb85
Irina K. Tezaur <ikalash@sandia.gov>
2016-12-19 10:42:14 -0800

    Updating cronjob scripts to copy results of nightly tests to software-login.sandia.gov, and submit the results to the SON CDash site, for Luca to be able to access:
    http://my.cdash.org/index.php?project=HOMMEXX

--- components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_hommexx_skybridge_serial.sh
+++ components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_hommexx_skybridge_serial.sh
@@ -13,6 +13,8 @@ LOG_FILE=$BASE_DIR/nightly_log_skybridgeHOMMEXXserial.txt
 
 eval "env TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR  ctest -VV -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
 
+scp -r /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20* ikalash@software-login.sandia.gov:/home/ikalash/hommexxCDash/SkybridgeHOMMEXXSerial
+
 #bash process_results_ctest.sh
 #bash send_email_ctest.sh
 
--- components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_trilinos_skybridge_serial.sh
+++ components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_trilinos_skybridge_serial.sh
@@ -21,3 +21,4 @@ LOG_FILE=$BASE_DIR/nightly_log_skybridgeTrilinosSerial.txt
 
 eval "env  TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
 
+scp -r /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20* ikalash@software-login.sandia.gov:/home/ikalash/hommexxCDash/SkybridgeTrilinosSerial
