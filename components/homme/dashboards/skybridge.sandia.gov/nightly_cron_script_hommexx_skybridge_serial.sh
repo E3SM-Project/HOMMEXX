d8a2e1820d06e9a42f34b436a6dffccae5810eb5
Irina K. Tezaur <ikalash@sandia.gov>
2016-12-22 15:59:22 -0800

    Modifying skybridge scripts to correctly send CDash results to SON CDash site (http://my.cdash.org/index.php?project=HOMMEXX).

--- components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_hommexx_skybridge_serial.sh
+++ components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_hommexx_skybridge_serial.sh
@@ -13,7 +13,9 @@ LOG_FILE=$BASE_DIR/nightly_log_skybridgeHOMMEXXserial.txt
 
 eval "env TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR  ctest -VV -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
 
+rm -rf /gpfs1/ikalash/hommexxCDash/SkybridgeHOMMEXXSerial/* 
 cp -r /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20* /gpfs1/ikalash/hommexxCDash/SkybridgeHOMMEXXSerial
+rm -rf /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20* 
 
 #bash process_results_ctest.sh
 #bash send_email_ctest.sh
--- components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_trilinos_skybridge_serial.sh
+++ components/homme/dashboards/skybridge.sandia.gov/nightly_cron_script_trilinos_skybridge_serial.sh
@@ -21,4 +21,6 @@ LOG_FILE=$BASE_DIR/nightly_log_skybridgeTrilinosSerial.txt
 
 eval "env  TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
 
+rm -rf /gpfs1/ikalash/hommexxCDash/SkybridgeTrilinosSerial/* 
 cp -r /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20* /gpfs1/ikalash/hommexxCDash/SkybridgeTrilinosSerial
+rm -rf /home/ikalash/nightlyHOMMEXXCDash/build/Testing/20*
