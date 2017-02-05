d02a2bf5c6f0ba44134dae183757f764b5e6a931
Irina Tezaur <ikalash@sandia.gov>
2017-02-05 15:21:56 -0700

    Checking in CDash scripts for Bowman machine.  Currently these only build Trilinos, not HOMMEXX.  I cannot test the HOMMEXX scripts, as these required TPLs in mdeakin's home directory, which I do not have permission to yet.

---
+++ components/homme/dashboards/bowman.sandia.gov/bowman_modules.sh
@@ -0,0 +1,8 @@
+module add intel/compilers/17.0.098 openmpi/1.10.4/intel/17.0.098 cmake/3.5.2 git/2.8.2
+module list 
+export CC=mpicc
+export CXX=mpicxx
+export FC=mpif90
+export CCLD=${CXX}
+export CXXLD=${CXX}
+export FCLD=${FC}
