6117857bf71fb527baac78fd079326f1459b9c47
Irina Tezaur <ikalash@sandia.gov>
2017-02-06 16:18:38 -0700

    Checking in cmake file for bowman CDash, which builds Trilinos separately from HOMMEXX.

---
+++ cmake/machineFiles/bowman_cdash.cmake
@@ -0,0 +1,20 @@
+# CMake initial cache file for Sandia's bowman
+# tested with stock intel & openmpi
+
+SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER mpicxx CACHE FILEPATH "")
+
+# Michael's prefix directory which contains HDF5, PNetCDF, NetCDF, and NetCDF Fortran
+#SET (MDEAKIN_PREFIX "/home/mdeakin/prefix" FILEPATH "")
+SET (MDEAKIN_PREFIX "/home/ikalash/mdeakin_prefix" FILEPATH "")
+
+SET (PNETCDF_DIR ${MDEAKIN_PREFIX} CACHE FILEPATH "")
+SET (NETCDF_DIR ${MDEAKIN_PREFIX} CACHE FILEPATH "")
+SET (HDF5_DIR ${MDEAKIN_PREFIX} CACHE FILEPATH "")
+SET (ZLIB_DIR $ENV{ZLIB_ROOT} CACHE FILEPATH "")
+
+SET (USE_QUEUING FALSE CACHE BOOL "")
+SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+
+SET (ENABLE_INTEL_PHI TRUE CACHE BOOL "")
