47d3e33c039072cedc8154a5a4de0b94cdd5723d
Irina Tezaur <ikalash@sandia.gov>
2017-02-11 21:26:13 -0700

    Updating Bowman CDash scripts.

--- dashboards/bowman.sandia.gov/send_email_ctest_openmp.sh
+++ dashboards/bowman.sandia.gov/send_email_ctest_openmp.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMME
 TTTTT=`grep "(Timeout)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 
-/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
+/bin/mail -s "HOMMEXX (master, Bowman, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
 
