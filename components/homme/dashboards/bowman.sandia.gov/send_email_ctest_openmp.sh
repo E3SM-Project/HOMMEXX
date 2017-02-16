b5b8ba8bb291060cc240b1668df7ad10aa617316
Irina Tezaur <ikalash@sandia.gov>
2017-02-15 17:30:59 -0700

    Update to send email script.

--- components/homme/dashboards/bowman.sandia.gov/send_email_ctest_openmp.sh
+++ components/homme/dashboards/bowman.sandia.gov/send_email_ctest_openmp.sh
@@ -2,10 +2,11 @@
 
 #source $1 
 
-TTT=`grep "(Failed)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
-TTTT=`grep "(Not Run)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
-TTTTT=`grep "(Timeout)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
-TT=`grep "...   Passed" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
+TTT=`grep "(Failed)" nightly_log_bowmanHOMMEXXopenmp.txt -c`
+TTTT=`grep "(Not Run)" nightly_log_bowmanHOMMEXXopenmp.txt -c`
+TTTTT=`grep "(Timeout)" nightly_log_bowmanHOMMEXXopenmp.txt -c`
+TT=`grep "...   Passed" nightly_log_bowmanHOMMEXXopenmp.txt -c`
 
-/bin/mail -s "HOMMEXX (master, Bowman, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
+mail -s "HOMMEXX (master, Bowman, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < results_hommexx_openmp
 
+#/bin/mail -s "HOMMEXX (master, Bowman, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" -r 'Irina Tezaur<ikalash@bowman.sandia.gov>' "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < results_hommexx_openmp
