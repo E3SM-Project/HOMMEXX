2b8b6a86efc048775b60363c0a42bf06576575d6
Luca Bertagna <lbertag@sandia.gov>
2017-05-30 11:52:58 -0600

    Removed unused tests and added sphere operators tests to share_ut.cpp
    I removed the subcell_div_fluxes test and the matrix-matrix test (which
    tested a routine only used in subcell_div_fluxes).
    
    I added tests for gradient, divergence and vorticity sphere.

--- components/homme/test_execs/share_ut/share_ut.F90
+++ components/homme/test_execs/share_ut/share_ut.F90
@@ -1,94 +1,119 @@
 module share_ut_mod
+  use derivative_mod_base, only : derivative_t
+  use element_mod        , only : element_t
+  use kinds              , only : real_kind
+  use dimensions_mod     , only : np
+
   implicit none
+
+  type(derivative_t) :: deriv
+  type(element_t)    :: elem
 
 contains
 
-  subroutine matrix_matrix_f90 (Aptr, Bptr, Cptr, trA, trB) bind(c)
-    use kinds          , only : real_kind
-    use dimensions_mod , only : np
-    use iso_c_binding  , only : c_ptr, c_int, c_f_pointer
-    !
-    ! Inputs
-    !
-    type (c_ptr),         intent(in) :: Aptr, Bptr, Cptr
-    integer (kind=c_int), intent(in) :: trA, trB
-    !
-    ! Locals
-    !
-    real (kind=real_kind), dimension(:,:), pointer :: A, B, C
-
-    call c_f_pointer(Aptr, A, [np,np])
-    call c_f_pointer(Bptr, B, [np,np])
-    call c_f_pointer(Cptr, C, [np,np])
-
-    if (trA==1) then
-      if (trB==1) then
-        C = MATMUL(TRANSPOSE(A),TRANSPOSE(B))
-      else
-        C = MATMUL(TRANSPOSE(A),B)
-      endif
-    else
-      if (trB==1) then
-        C = MATMUL(A,TRANSPOSE(B))
-      else
-        C = MATMUL(A,B)
-      endif
-    endif
-  end subroutine matrix_matrix_f90
-
-  subroutine init_deriv_arrays_f90 (dvv_ptr, integ_mat_ptr, bd_interp_mat_ptr) bind(c)
-    use derivative_mod_base , only : integration_matrix, boundary_interp_matrix, &
-                                     derivative_t, derivinit,                    &
-                                     allocate_subcell_integration_matrix
-    use dimensions_mod      , only : np, nc
-    use kinds               , only : real_kind
+  subroutine init_deriv_f90 (dvv_ptr) bind(c)
+    use derivative_mod_base , only : derivinit
     use iso_c_binding       , only : c_ptr, c_f_pointer
     !
     ! Inputs
     !
-    type (c_ptr), intent(in) :: dvv_ptr, integ_mat_ptr, bd_interp_mat_ptr
+    type (c_ptr), intent(in) :: dvv_ptr
     !
     ! Locals
     !
     real (kind=real_kind), dimension(:,:),   pointer :: dvv
-    real (kind=real_kind), dimension(:,:),   pointer :: integ_mat
-    real (kind=real_kind), dimension(:,:,:), pointer :: bd_interp_mat
-    type (derivative_t) :: deriv
 
     call c_f_pointer (dvv_ptr,           dvv,           [np, np])
-    call c_f_pointer (integ_mat_ptr,     integ_mat,     [nc, np])
-    call c_f_pointer (bd_interp_mat_ptr, bd_interp_mat, [nc, 2, np])
 
-    call allocate_subcell_integration_matrix (np, nc)
     call derivinit (deriv)
 
     dvv = deriv%dvv
-    integ_mat = integration_matrix
-    bd_interp_mat = boundary_interp_matrix
 
-  end subroutine init_deriv_arrays_f90
+  end subroutine init_deriv_f90
 
-  subroutine subcell_div_fluxes_f90 (u_ptr, metdet_ptr, flux_ptr) bind(c)
-    use kinds               , only : real_kind
-    use dimensions_mod      , only : np, nc
-    use derivative_mod_base , only : subcell_div_fluxes
+  subroutine gradient_sphere_f90 (scalar_ptr, DInv_ptr, vector_ptr, nelems) bind(c)
+    use derivative_mod_base , only : gradient_sphere
     use iso_c_binding       , only : c_ptr, c_int, c_f_pointer
     !
     ! Inputs
     !
-    type (c_ptr),         intent(in) :: u_ptr, metdet_ptr, flux_ptr
+    integer (kind=c_int), intent(in) :: nelems
+    type (c_ptr)        , intent(in) :: scalar_ptr, DInv_ptr, vector_ptr
     !
     ! Locals
     !
-    real (kind=real_kind), dimension(:,:),   pointer :: metdet
-    real (kind=real_kind), dimension(:,:,:), pointer :: u, flux
+    real (kind=real_kind), dimension(:,:,:),     pointer :: scalar
+    real (kind=real_kind), dimension(:,:,:,:),   pointer :: vector
+    real (kind=real_kind), dimension(:,:,:,:,:), pointer :: DInv
+    integer :: ie
 
-    call c_f_pointer(u_ptr,      u,      [np, np, 2])
-    call c_f_pointer(metdet_ptr, metdet, [np, np])
-    call c_f_pointer(flux_ptr,   flux,   [nc, nc, 4])
+    call c_f_pointer(scalar_ptr, scalar, [np, np, nelems])
+    call c_f_pointer(DInv_ptr,   DInv,   [np, np, 2, 2, nelems])
+    call c_f_pointer(vector_ptr, vector, [np, np, 2, nelems])
 
-    flux = subcell_div_fluxes(u,np,nc,metdet)
+    do ie=1,nelems
+      vector(:,:,:,ie) = gradient_sphere(scalar(:,:,ie),deriv,DInv(:,:,:,:,ie))
+    enddo
+  end subroutine gradient_sphere_f90
 
-  end subroutine subcell_div_fluxes_f90
+  subroutine divergence_sphere_f90 (vector_ptr, DInv_ptr, metdet_ptr, div_ptr, nelems) bind(c)
+    use derivative_mod_base , only : divergence_sphere
+    use iso_c_binding       , only : c_ptr, c_int, c_f_pointer
+    !
+    ! Inputs
+    !
+    type (c_ptr)        , intent(in) :: vector_ptr, DInv_ptr, metdet_ptr, div_ptr
+    integer (kind=c_int), intent(in) :: nelems
+    !
+    ! Locals
+    !
+    real (kind=real_kind), dimension(:,:,:,:),   pointer :: vector
+    real (kind=real_kind), dimension(:,:,:,:,:), pointer :: DInv
+    real (kind=real_kind), dimension(:,:,:),     pointer :: metdet
+    real (kind=real_kind), dimension(:,:,:),     pointer :: div
+    integer :: ie
+
+    call c_f_pointer(DInv_ptr,   DInv,   [np, np, 2, 2, nelems])
+    call c_f_pointer(vector_ptr, vector, [np, np, 2, nelems])
+    call c_f_pointer(metdet_ptr, metdet, [np, np, nelems])
+    call c_f_pointer(div_ptr,    div,    [np, np, nelems])
+
+    do ie=1,nelems
+      elem%DInv   = DInv(:,:,:,:,ie)
+      elem%metdet = metdet(:,:,ie)
+
+      div(:,:,ie) = divergence_sphere(vector(:,:,:,ie),deriv,elem)
+    enddo
+  end subroutine divergence_sphere_f90
+
+  subroutine vorticity_sphere_f90 (vector_ptr, D_ptr, metdet_ptr, vort_ptr, nelems) bind(c)
+    use derivative_mod_base , only : vorticity_sphere
+    use iso_c_binding       , only : c_ptr, c_int, c_f_pointer
+    !
+    ! Inputs
+    !
+    type (c_ptr)        , intent(in) :: vector_ptr, D_ptr, metdet_ptr, vort_ptr
+    integer (kind=c_int), intent(in) :: nelems
+    !
+    ! Locals
+    !
+    real (kind=real_kind), dimension(:,:,:,:),   pointer :: vector
+    real (kind=real_kind), dimension(:,:,:,:,:), pointer :: D
+    real (kind=real_kind), dimension(:,:,:),     pointer :: metdet
+    real (kind=real_kind), dimension(:,:,:),     pointer :: vort
+    integer :: ie
+
+    call c_f_pointer(D_ptr,      D,      [np, np, 2, 2, nelems])
+    call c_f_pointer(vector_ptr, vector, [np, np, 2, nelems])
+    call c_f_pointer(metdet_ptr, metdet, [np, np, nelems])
+    call c_f_pointer(vort_ptr,   vort,   [np, np, nelems])
+
+    do ie=1,nelems
+      elem%D      = D(:,:,:,:,ie)
+      elem%metdet = metdet(:,:,ie)
+
+      vort(:,:,ie) = vorticity_sphere(vector(:,:,:,ie),deriv,elem)
+    enddo
+  end subroutine vorticity_sphere_f90
 
 end module share_ut_mod
