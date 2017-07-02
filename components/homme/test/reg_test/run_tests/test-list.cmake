9e6b52c123d3ddb8b6253a44e21ccfbe35f08e7d
Oksana Guba <oksana.guba@gmail.com>
2017-07-02 15:32:09 -0600

    Disabling SW tests since they use different from 3D kokkos kernels; cleaning tests list some.

--- components/homme/test/reg_test/run_tests/test-list.cmake
+++ components/homme/test/reg_test/run_tests/test-list.cmake
@@ -1,26 +1,26 @@
 # Lists of test files for the HOMME regression tests
 SET(HOMME_TESTS
-  swirl.cmake
-  swtc1_flat.cmake
-  swtc1_flat_c.cmake
-  swtc2_flat.cmake
-  swtc2_flat_c.cmake
-  swtc5_flat.cmake
-  swtc5_flat_c.cmake
-  swtc6_flat.cmake
-  swtc6_flat_c.cmake
-  swirl_flat.cmake
-  swirl_flat_c.cmake
+#  swirl.cmake
+#  swtc1_flat.cmake
+#  swtc1_flat_c.cmake
+#  swtc2_flat.cmake
+#  swtc2_flat_c.cmake
+#  swtc5_flat.cmake
+#  swtc5_flat_c.cmake
+#  swtc6_flat.cmake
+#  swtc6_flat_c.cmake
+#  swirl_flat.cmake
+#  swirl_flat_c.cmake
   prtcB_flat.cmake
   prtcB_flat_c.cmake
-  baro2b.cmake
-  baro2c.cmake
-  baro2d.cmake
-  baroCamMoist.cmake
-  baroCamMoist-SL.cmake
-  baroCamMoist-acc.cmake
-  baro2d-imp.cmake
-  templates.cmake
+#  baro2b.cmake
+#  baro2c.cmake
+#  baro2d.cmake
+#  baroCamMoist.cmake
+#  baroCamMoist-SL.cmake
+#  baroCamMoist-acc.cmake
+#  baro2d-imp.cmake
+#  templates.cmake
 )
 
 SET (HOMMEXX_TESTS
