6a997e8e58ae104f560d170efceb0a6dc68b5ee9
Luca Bertagna <lbertag@sandia.gov>
2017-07-19 14:28:53 -0600

    Removed unused stuff relative to subcell_div_fluxes.

--- src/preqx_flat/derivative_mod.F90
+++ src/preqx_flat/derivative_mod.F90
@@ -7,6 +7,6 @@ module derivative_mod
                                  derivinit, deriv_print, gradient, gradient_wk, vorticity, divergence, interpolate_gll2fvm_corners, interpolate_gll2fvm_points, &
                                  remap_phys2gll, v2pinit, gradient_sphere_wk_testcov, gradient_sphere_wk_testcontra, ugradv_sphere, vorticity_sphere, vorticity_sphere_diag, curl_sphere,     &
                                  curl_sphere_wk_testcov, vlaplace_sphere_wk, element_boundary_integral, edge_flux_u_cg, limiter_optim_iter_full, &
-                                 laplace_sphere_wk, divergence_sphere_wk, gradient_sphere, divergence_sphere, integration_matrix, boundary_interp_matrix
+                                 laplace_sphere_wk, divergence_sphere_wk, gradient_sphere, divergence_sphere
   implicit none
 end module derivative_mod
