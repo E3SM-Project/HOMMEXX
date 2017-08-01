72ba95c9a4d83539636c85bd157c7fcadd395207
Luca Bertagna <lbertag@sandia.gov>
2017-08-01 16:58:46 -0600

    Removed unused routines from Utility.hpp and added some subview utilities.

--- src/share/cxx/Utility.hpp
+++ src/share/cxx/Utility.hpp
@@ -5,67 +5,25 @@
 
 namespace Homme {
 
-namespace Impl
+template<typename MemSpace, typename ScalarType>
+ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>
+subview(ViewUnmanaged<ScalarType*[NP][NP][NUM_LEV],MemSpace> v_in, int ie)
 {
+  return ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>(&v_in(ie,0,0,0));
+}
 
-// No generic implementation
-template<typename ViewOut, typename ViewIn, bool DoCopy>
-struct DeepCopyImpl;
-
-// Specialization for deep copy necessary
-template<typename ViewOut, typename ViewIn>
-struct DeepCopyImpl<ViewOut, ViewIn, true>
+template<typename MemSpace, typename ScalarType>
+ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>
+subview(ViewUnmanaged<ScalarType*[NUM_TIME_LEVELS][NP][NP][NUM_LEV],MemSpace> v_in, int ie, int itl)
 {
-  KOKKOS_FORCEINLINE_FUNCTION
-  static void copy(ViewOut view_out, ViewIn view_in)
-  {
-    // Note: Kokkos will do some compile-time checks
-    Kokkos::deep_copy(view_out, view_in);
-  }
-};
+  return ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>(&v_in(ie,itl,0,0,0));
+}
 
-// Specialization for deep copy not necessary
-template<typename ViewOut, typename ViewIn>
-struct DeepCopyImpl<ViewOut, ViewIn, false>
+template<typename MemSpace, typename ScalarType>
+ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>
+subview(ViewUnmanaged<ScalarType*[Q_NUM_TIME_LEVELS][QSIZE_D][NP][NP][NUM_LEV],MemSpace> v_in, int ie, int itl, int iq)
 {
-  static void copy(ViewOut /*view_out*/, ViewIn /*view_in*/)
-  {
-    // At the very least, We can check that the underlying data type is the same
-    // (apart from possible cv-qualifiers). This can help finding bugs at compile-time
-    static_assert (std::is_same<typename ViewIn::non_const_data_type,
-                                typename ViewOut::non_const_data_type
-                               >::value,
-                   "Error! Trying to copy views with intrinsically (not just cv-related) incompatible data types.");
-  }
-};
-
-} // namespace Impl
-
- /*
-  * This function will actually perform a deep copy only if the memory spaces
-  * are different. Notice that this means that calling this function with two
-  * views living on the same memory space will always end up in a no-op, even
-  * if the two views are 'different'. Use this method only to perform a deep
-  * copy between a view and its mirror view on (possibly) another memory space.
-  * For all the other cases, use Kokkos::deep_copy.
-  * Why we need this? Glad you asked. Right now, Kokkos::deep_copy has a compile
-  * time check to ensure that the data type of the destination view is NOT const.
-  * This can be a problem if you create view_out using create_mirror_view, passing
-  * the output memory space. In particular, if you have a View view_in with
-  * const data type on memory space MS, and you create a mirror view on the same
-  * memory space (perhaps you're using OpenMP), the output view will be a shallow
-  * copy of view_in, with also const data type. If you then try call deep_copy
-  * on these two views, the compiler will bite you.
-  */
-template<typename ViewOut, typename ViewIn>
-KOKKOS_FORCEINLINE_FUNCTION
-void deep_copy_mirror_view(ViewOut view_out, ViewIn view_in)
-{
-  static constexpr bool do_copy = !std::is_same<typename ViewOut::memory_space,
-                                                typename ViewIn::memory_space
-                                               >::value;
-
-  Impl::DeepCopyImpl<ViewOut,ViewIn,do_copy>::copy(view_out, view_in);
+  return ViewUnmanaged<ScalarType [NP][NP][NUM_LEV],MemSpace>(&v_in(ie,itl,iq,0,0,0));
 }
 
 template<typename ViewType>
