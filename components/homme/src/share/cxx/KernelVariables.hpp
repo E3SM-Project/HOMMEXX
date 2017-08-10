3bc54dd98564e73578a2861bdc174dfc820dae91
Michael Deakin <mdeakin@sandia.gov>
2017-08-10 16:10:49 -0600

    Remove shared memory usage

--- components/homme/src/share/cxx/KernelVariables.hpp
+++ components/homme/src/share/cxx/KernelVariables.hpp
@@ -8,8 +8,8 @@ namespace Homme {
 struct KernelVariables {
   KOKKOS_INLINE_FUNCTION
   KernelVariables(const TeamMember &team_in)
-      : team(team_in), scratch_mem(allocate_thread<Real, Real[NP][NP]>()),
-        ie(team.league_rank()), ilev(-1) {} //, igp(-1), jgp(-1) {}
+      : team(team_in), ie(team.league_rank()), ilev(-1) {
+  } //, igp(-1), jgp(-1) {}
 
   template <typename Primitive, typename Data>
   KOKKOS_INLINE_FUNCTION Primitive *allocate_team() const {
@@ -25,14 +25,11 @@ struct KernelVariables {
 
   KOKKOS_INLINE_FUNCTION
   static size_t shmem_size(int team_size) {
-    size_t mem_size = sizeof(Real[NP][NP]) * team_size;
+    size_t mem_size = 0 * team_size;
     return mem_size;
   }
 
   const TeamMember &team;
-
-  // Fast memory for the kernel
-  ExecViewUnmanaged<Real[NP][NP]> scratch_mem;
 
   int ie, ilev;
 }; // KernelVariables
