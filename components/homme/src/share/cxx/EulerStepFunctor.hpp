db263a6d971b279d6d4ff0819cbf4db2f1bd3075
Luca Bertagna <lbertag@sandia.gov>
2017-08-14 14:12:09 -0600

    Adjusted CaarFunctor and EulerStepFunctor to work with unpacked buffer views.
    Also, replaced ::Homme::subview with Homme::subview (the leading :: was
    superfluous).

--- components/homme/src/share/cxx/EulerStepFunctor.hpp
+++ components/homme/src/share/cxx/EulerStepFunctor.hpp
@@ -15,14 +15,6 @@ struct EulerStepFunctor
   const Region      m_region;
   const Derivative  m_deriv;
 
-  static constexpr int IDX_USTAR = 0;
-  static constexpr int IDX_VSTAR = 1;
-
-  static constexpr int IDX_QBUFF = 0;
-  static constexpr int IDX_UBUFF = 1;
-  static constexpr int IDX_VBUFF = 2;
-
-
   EulerStepFunctor (const Control& data)
    : m_data    (data)
    , m_region  (get_region())
@@ -42,11 +34,8 @@ struct EulerStepFunctor
   {
     KernelVariables kv(team);
 
-    ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]> ustar = ::Homme::subview(m_region.buffers.scalars,kv.ie,IDX_USTAR);
-    ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]> vstar = ::Homme::subview(m_region.buffers.scalars,kv.ie,IDX_VSTAR);
-
-    ExecViewUnmanaged<const Real[2][2][NP][NP]> dinv   = ::Homme::subview(m_region.m_dinv,kv.ie);
-    ExecViewUnmanaged<const Real[NP][NP]>       metdet = ::Homme::subview(m_region.m_metdet,kv.ie);
+    ExecViewUnmanaged<const Real[2][2][NP][NP]> dinv   = Homme::subview(m_region.m_dinv,kv.ie);
+    ExecViewUnmanaged<const Real[NP][NP]>       metdet = Homme::subview(m_region.m_metdet,kv.ie);
 
     Kokkos::parallel_for (
       Kokkos::TeamThreadRange(team,NUM_LEV*m_data.qsize),
@@ -55,11 +44,9 @@ struct EulerStepFunctor
         const int iq   = lev_q / NUM_LEV;
         kv.ilev = lev_q % NUM_LEV;
 
-        ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]> qdp   = ::Homme::subview(m_region.m_qdp,kv.ie,m_data.qn0,iq);
-        ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>       q_buf = ::Homme::subview(m_region.buffers.tracers,kv.ie,IDX_QBUFF,iq);
-        ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>       u_buf = ::Homme::subview(m_region.buffers.tracers,kv.ie,IDX_UBUFF,iq);
-        ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>       v_buf = ::Homme::subview(m_region.buffers.tracers,kv.ie,IDX_VBUFF,iq);
-
+        ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]> qdp   = Homme::subview(m_region.m_qdp,kv.ie,m_data.qn0,iq);
+        ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>       q_buf = Homme::subview(m_region.buffers.qtens,kv.ie,iq);
+        ExecViewUnmanaged<Scalar[2][NP][NP][NUM_LEV]>    v_buf = Homme::subview(m_region.buffers.vstar_qdp,kv.ie,iq);
 
         Kokkos::parallel_for (
           Kokkos::ThreadVectorRange (team, NP*NP),
@@ -68,14 +55,14 @@ struct EulerStepFunctor
             const int igp = idx / NP;
             const int jgp = idx % NP;
 
-            u_buf(igp,jgp,kv.ilev) = ustar(igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
-            v_buf(igp,jgp,kv.ilev) = vstar(igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
+            v_buf(0,igp,jgp,kv.ilev) = m_region.buffers.vstar(kv.ie,0,igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
+            v_buf(1,igp,jgp,kv.ilev) = m_region.buffers.vstar(kv.ie,1,igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
             q_buf(igp,jgp,kv.ilev) = qdp(igp,jgp,kv.ilev);
           }
         );
 
         divergence_sphere_update(kv, -m_data.dt, 1.0, dinv, metdet,
-                                 m_deriv.get_dvv(), u_buf, v_buf, q_buf);
+                                 m_deriv.get_dvv(), v_buf, q_buf);
       }
     );
   }
