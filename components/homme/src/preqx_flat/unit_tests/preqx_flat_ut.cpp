dd0e5debb1f297ed77d91ced49b9eda82486b945
Michael Deakin <mdeakin@sandia.gov>
2017-08-24 11:54:29 -0600

    Implement most of unit test for computing dp3d

--- components/homme/src/preqx_flat/unit_tests/preqx_flat_ut.cpp
+++ components/homme/src/preqx_flat/unit_tests/preqx_flat_ut.cpp
@@ -34,6 +34,12 @@ void preq_omega_ps_c_int(Real *omega_p,
                          const Real *dinv,
                          const Real *dvv);
 
+void caar_compute_dp3d_np1_c_int(const int &np1, const int &nm1,
+                                 const Real &dt2,
+                                 const Real *spheremp,
+                                 const Real *divdp,
+                                 const Real *eta_dot_dpdn,
+                                 Real *dp3d);
 }  // extern C
 
 /* compute_subfunctor_test
@@ -48,34 +54,35 @@ void preq_omega_ps_c_int(Real *omega_p,
 template <typename TestFunctor_T>
 class compute_subfunctor_test {
  public:
-  compute_subfunctor_test(int num_elems)
+  compute_subfunctor_test(Elements &elements)
       : functor(),
-        velocity("Velocity", num_elems),
-        temperature("Temperature", num_elems),
-        dp3d("DP3D", num_elems),
-        phi("Phi", num_elems),
-        pecnd("PE_CND", num_elems),
-        omega_p("Omega_P", num_elems),
-        derived_v("Derived V?", num_elems),
-        eta_dpdn("Eta dot dp/deta", num_elems),
-        qdp("QDP", num_elems),
-        dinv("DInv", num_elems),
+        velocity("Velocity", elements.num_elems()),
+        temperature("Temperature", elements.num_elems()),
+        dp3d("DP3D", elements.num_elems()),
+        phi("Phi", elements.num_elems()),
+        pecnd("PE_CND", elements.num_elems()),
+        omega_p("Omega_P", elements.num_elems()),
+        derived_v("Derived V?", elements.num_elems()),
+        eta_dpdn("Eta dot dp/deta", elements.num_elems()),
+        qdp("QDP", elements.num_elems()),
+        dinv("DInv", elements.num_elems()),
+        spheremp("SphereMP", elements.num_elems()),
         dvv("dvv"),
         nets(1),
-        nete(num_elems) {
+        nete(elements.num_elems()) {
     Real hybrid_a[NUM_LEV_P] = {0};
-    functor.m_data.init(0, num_elems, num_elems, nm1, n0,
-                        np1, qn0, ps0, dt2, false,
+    functor.m_data.init(0, region.num_elems(), region.num_elems(),
+                        nm1, n0, np1, qn0, ps0, dt2, false,
                         eta_ave_w, hybrid_a);
 
     get_derivative().dvv(dvv.data());
 
-    get_elements().push_to_f90_pointers(
+    elements.push_to_f90_pointers(
         velocity.data(), temperature.data(), dp3d.data(),
         phi.data(), pecnd.data(), omega_p.data(),
         derived_v.data(), eta_dpdn.data(), qdp.data());
     for(int ie = 0; ie < num_elems; ++ie) {
-      get_elements().dinv(
+      elements.dinv(
           Kokkos::subview(dinv, ie, Kokkos::ALL,
                           Kokkos::ALL, Kokkos::ALL,
                           Kokkos::ALL)
@@ -128,33 +135,41 @@ class compute_subfunctor_test {
                       [QSIZE_D][NUM_PHYSICAL_LEV][NP][NP]>
       qdp;
   HostViewManaged<Real * [2][2][NP][NP]> dinv;
+  HostViewManaged<Real * [NP][NP]> spheremp;
   HostViewManaged<Real[NP][NP]> dvv;
 
   const int nets;
   const int nete;
 
-  static constexpr const int nm1 = 0;
-  static constexpr const int n0 = 1;
-  static constexpr const int np1 = 2;
-  static constexpr const int qn0 = -1;
-  static constexpr const int ps0 = 1;
-  static constexpr const Real dt2 = 1.0;
-  static constexpr const Real eta_ave_w = 1.0;
+  static constexpr int nm1 = 0;
+  static constexpr int n0 = 1;
+  static constexpr int np1 = 2;
+  static constexpr int qn0 = -1;
+  static constexpr int ps0 = 1;
+  static constexpr Real dt2 = 1.0;
+  static constexpr Real eta_ave_w = 1.0;
 };
 
-class compute_energy_grad_test {
- public:
-  KOKKOS_INLINE_FUNCTION
-  static void test_functor(const CaarFunctor &functor,
-                           KernelVariables &kv) {
-    Kokkos::parallel_for(
-        Kokkos::TeamThreadRange(kv.team, NUM_LEV),
-        [&](const int &level) {
-          kv.ilev = level;
-          functor.compute_energy_grad(kv);
-        });
+void sync_to_host(ExecViewUnmanaged<Scalar *[NUM_TIME_LEVELS][NP][NP][NUM_LEV]> source,
+                  HostViewUnmanaged<Real *[NUM_TIME_LEVELS][NUM_PHYSICAL_LEV][NP][NP]> dest) {
+  ExecViewUnmanaged<Scalar *[NUM_TIME_LEVELS][NP][NP][NUM_LEV]>::HostMirror source_mirror =
+      Kokkos::create_mirror_view(source);
+  Kokkos::deep_copy(source_mirror, source);
+  for(int ie = 0; ie < source.extent_int(0); ++ie) {
+    for(int time = 0; time < NUM_TIME_LEVELS; ++time) {
+      for(int vector_level = 0, level = 0; vector_level < NUM_LEV; ++vector_level) {
+        for(int vector = 0; vector < VECTOR_SIZE; ++vector, ++level) {
+          for(int igp = 0; igp < NP; ++igp) {
+            for(int jgp = 0; jgp < NP; ++jgp) {
+              dest(ie, time, level, igp, jgp) =
+                source_mirror(ie, time, igp, jgp, vector_level)[vector];
+            }
+          }
+        }
+      }
+    }
   }
-};
+}
 
 void sync_to_device(HostViewUnmanaged<Real *[NUM_PHYSICAL_LEV][NP][NP]> source,
                     ExecViewUnmanaged<Scalar *[NP][NP][NUM_LEV]> dest) {
@@ -175,6 +190,20 @@ void sync_to_device(HostViewUnmanaged<Real *[NUM_PHYSICAL_LEV][NP][NP]> source,
   Kokkos::deep_copy(dest, dest_mirror);
 }
 
+class compute_energy_grad_test {
+ public:
+  KOKKOS_INLINE_FUNCTION
+  static void test_functor(const CaarFunctor &functor,
+                           KernelVariables &kv) {
+    Kokkos::parallel_for(
+        Kokkos::TeamThreadRange(kv.team, NUM_LEV),
+        [&](const int &level) {
+          kv.ilev = level;
+          functor.compute_energy_grad(kv);
+        });
+  }
+};
+
 TEST_CASE("compute_energy_grad", "monolithic compute_and_apply_rhs") {
   constexpr const Real rel_threshold =
       std::numeric_limits<Real>::epsilon() * 128.0;
@@ -190,7 +219,7 @@ TEST_CASE("compute_energy_grad", "monolithic compute_and_apply_rhs") {
   get_derivative().random_init(engine);
 
   compute_subfunctor_test<compute_energy_grad_test>
-      test_functor(num_elems);
+      test_functor(region);
   test_functor.run_functor();
   HostViewManaged<Scalar * [2][NP][NP][NUM_LEV]>
       energy_grad("energy_grad", num_elems);
@@ -277,8 +306,9 @@ TEST_CASE("preq_omega_ps", "monolithic compute_and_apply_rhs") {
                std::uniform_real_distribution<Real>(0, 100.0));
   sync_to_device(div_vdp, region.buffers.div_vdp);
 
-  compute_subfunctor_test<preq_omega_ps_test> test_functor(num_elems);
+  compute_subfunctor_test<preq_omega_ps_test> test_functor(region);
   test_functor.run_functor();
+  // Results of the computation
   HostViewManaged<Scalar * [NP][NP][NUM_LEV]> omega_p("omega_p", num_elems);
   Kokkos::deep_copy(omega_p, region.buffers.omega_p);
 
@@ -313,3 +343,59 @@ TEST_CASE("preq_omega_ps", "monolithic compute_and_apply_rhs") {
   }
 }
 
+class dp3d_test {
+public:
+  KOKKOS_INLINE_FUNCTION
+  static void test_functor(const CaarFunctor &functor, KernelVariables &kv) {
+    if(kv.team.team_rank() == 0) {
+      functor.compute_dp3d_np1(kv);
+    }
+  }
+};
+
+TEST_CASE("dp3d", "monolithic compute_and_apply_rhs") {
+  constexpr const Real rel_threshold =
+      std::numeric_limits<Real>::epsilon() * 128.0;
+  constexpr const int num_elems = 10;
+
+  std::random_device rd;
+  rngAlg engine(rd());
+
+  // This must be a reference to ensure the views are initialized in the
+  // singleton
+  CaarRegion &region = get_region();
+  region.random_init(num_elems, engine);
+  get_derivative().random_init(engine);
+
+  HostViewManaged<Real * [NUM_PHYSICAL_LEV][NP][NP]> div_vdp("host div_vdp", num_elems);
+  genRandArray(reinterpret_cast<Real *>(div_vdp.data()),
+               div_vdp.span(), engine,
+               std::uniform_real_distribution<Real>(0, 100.0));
+  sync_to_device(div_vdp, region.buffers.div_vdp);
+
+  compute_subfunctor_test<dp3d_test> test_functor(region);
+  test_functor.run_functor();
+  sync_to_host(region.m_dp3d, test_functor.dp3d);
+
+  HostViewManaged<Real *[NUM_TIME_LEVELS][NUM_PHYSICAL_LEV][NP][NP]> dp3d;
+  for(int ie = 0; ie < num_elems; ++ie) {
+    caar_compute_dp3d_np1_c_int(test_functor.functor.m_data.np1,
+                                test_functor.functor.m_data.nm1,
+                                test_functor.functor.m_data.dt2,
+                                test_functor.spheremp.data(), div_vdp.data(),
+                                test_functor.eta_dpdn.data(), dp3d.data());
+    for(int k = 0; k < NUM_PHYSICAL_LEV; ++k) {
+      for(int igp = 0; igp < NP; ++igp) {
+        for(int jgp = 0; jgp < NP; ++jgp) {
+          REQUIRE(!std::isnan(dp3d(ie, test_functor.functor.m_data.np1, k, igp, jgp)));
+          REQUIRE(!std::isnan(test_functor.dp3d(ie, test_functor.functor.m_data.np1, k, igp,
+                                                jgp)));
+          Real rel_error = compare_answers(test_functor.dp3d(ie, test_functor.functor.m_data.np1,
+                                                             k, igp, jgp),
+                                           dp3d(ie, test_functor.functor.m_data.np1, k, igp, jgp));
+          REQUIRE(rel_threshold >= rel_error);
+        }
+      }
+    }
+  }
+}
