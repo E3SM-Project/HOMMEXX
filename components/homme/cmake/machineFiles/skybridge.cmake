b18200260e590d1ee09f449bb76575bd12b0a485
Michael Deakin <mdeakin@sandia.gov>
2017-08-31 10:52:20 -0600

    Fix hdf5 and netcdf dir settings for the new modules on Skybridge

--- components/homme/cmake/machineFiles/skybridge.cmake
+++ components/homme/cmake/machineFiles/skybridge.cmake
@@ -24,8 +24,8 @@ SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
 #SET (FORCE_Fortran_FLAGS "-openmp -fp-model fast -ftz -g -O2" CACHE STRING "")
 SET (FORCE_Fortran_FLAGS "-openmp -traceback -fp-model precise -ftz -g -O2" CACHE STRING "")
 
-SET (NETCDF_DIR /projects/ccsm/tpl/netcdf/4.3.2/intel/13.0.1/openmpi/1.6.5 CACHE FILEPATH "")
-SET (HDF5_DIR /projects/ccsm/tpl/hdf5/1.8.11/intel/13.0.1/openmpi/1.6.5 CACHE FILEPATH "")
+SET (NETCDF_DIR $ENV{SEMS_NETCDF_ROOT} CACHE FILEPATH "")
+SET (HDF5_DIR $ENV{SEMS_HDF5_ROOT} CACHE FILEPATH "")
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
