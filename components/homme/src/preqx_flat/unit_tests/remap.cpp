f7934cf708615137a207179cfa6cae8746074844
Oksana Guba <oksanaguba@gmail.com>
2017-09-08 08:39:19 -0600

    More work for remap stashed.

---
+++ components/homme/src/preqx_flat/unit_tests/remap.cpp
@@ -0,0 +1,32 @@
+#ifndef REMAP_CPP
+#define REMAP_CPP
+
+#include <catch/catch.hpp>
+
+#include <limits>
+
+//#include "CaarControl.hpp"
+//#include "CaarFunctor.hpp"
+//#include "CaarRegion.hpp"
+#include "Dimensions.hpp"
+//#include "KernelVariables.hpp"
+#include "Types.hpp"
+
+#include <assert.h>
+#include <stdio.h>
+#include <random>
+
+using namespace Homme;
+
+void compute_ppm_grids(const Real *){
+
+
+
+
+
+}
+
+
+
+
+
