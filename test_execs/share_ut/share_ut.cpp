e348b13bb746bc7fbe6db9515b1bdda45bbd2950
Michael Deakin <mdeakin@sandia.gov>
2017-09-14 16:13:25 -0600

    Cleanups - use the view based genRandArray, and share both genRandArray and compare_answers in Utilities.hpp (maybe should go in TestUtilities.hpp?)

--- test_execs/share_ut/share_ut.cpp
+++ test_execs/share_ut/share_ut.cpp
@@ -29,27 +29,6 @@ void vorticity_sphere_c_callable(const Real *vector, const Real *dvv,
 
 } // extern "C"
 
-template <typename rngAlg, typename PDF>
-void genRandArray(Real *const x, int length, rngAlg &engine, PDF &pdf) {
-  for (int i = 0; i < length; ++i) {
-    x[i] = pdf(engine);
-  }
-}
-
-template <typename ViewType, typename rngAlg, typename PDF>
-void genRandArray(ViewType view, rngAlg &engine, PDF &pdf) {
-  genRandArray(view.data(), view.size(), engine, pdf);
-}
-
-Real compare_answers(Real target, Real computed, Real relative_coeff = 1.0) {
-  Real denom = 1.0;
-  if (relative_coeff > 0.0 && target != 0.0) {
-    denom = relative_coeff * std::fabs(target);
-  }
-
-  return std::fabs(target - computed) / denom;
-}
-
 // ================================= TESTS ============================ //
 
 TEST_CASE("flip_arrays", "flip arrays routines") {
