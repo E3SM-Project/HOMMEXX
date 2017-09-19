08cc2bd67d458b669efc26041863589aa6e85eaf
Irina K. Tezaur <ikalash@sandia.gov>
2017-09-19 12:02:15 -0600

    Adding Andrew Bradley to list of people receiving CDash nightly test emails on Skybridge.

--- components/homme/dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
+++ components/homme/dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHO
 TTTTT=`grep "(Timeout)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
 
-/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
+/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gv" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
 
