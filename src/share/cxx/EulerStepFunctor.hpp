8c51cc34d3f2f73425967492c824aa05f6b30bb9
Andrew M. Bradley <ambradl@sandia.gov>
2017-10-19 13:56:38 -0600

    Partly fix #80.
    Fix nested threading issue in EulerStepFunctor.
    
    Fix threading bug in divergence_sphere_update.
    
    This commit does nota address the issue of threading over tracers, GLL points,
    and possibly level packs simultaneously. Its focus is just correctness.

--- src/share/cxx/EulerStepFunctor.hpp
+++ src/share/cxx/EulerStepFunctor.hpp
@@ -12,12 +12,12 @@ namespace Homme
 struct EulerStepFunctor
 {
   const Control     m_data;
-  const Elements      m_elements;
+  const Elements    m_elements;
   const Derivative  m_deriv;
 
   EulerStepFunctor (const Control& data)
    : m_data    (data)
-   , m_elements  (get_elements())
+   , m_elements(get_elements())
    , m_deriv   (get_derivative())
   {
     // Nothing to be done here
@@ -30,41 +30,48 @@ struct EulerStepFunctor
   }
 
   KOKKOS_INLINE_FUNCTION
-  void operator() (TeamMember team) const
-  {
+  void operator() (const TeamMember& team) const {
     KernelVariables kv(team);
 
-    ExecViewUnmanaged<const Real[2][2][NP][NP]> dinv   = Homme::subview(m_elements.m_dinv,kv.ie);
-    ExecViewUnmanaged<const Real[NP][NP]>       metdet = Homme::subview(m_elements.m_metdet,kv.ie);
+    using Kokkos::ALL;
+    const ExecViewUnmanaged<const Real[NP][NP]>
+      metdet = Kokkos::subview(m_elements.m_metdet, kv.ie, ALL, ALL);
 
+    const auto NP2 = NP * NP;
     Kokkos::parallel_for (
-      Kokkos::TeamThreadRange(team,NUM_LEV*m_data.qsize),
-      [&] (const int lev_q)
-      {
-        const int iq   = lev_q / NUM_LEV;
-        kv.ilev = lev_q % NUM_LEV;
+      Kokkos::TeamThreadRange(team, NP2*m_data.qsize),
+      [&] (const int loop_idx) {
+        const int iq  = loop_idx / NP2;
+        const int igp = (loop_idx % NP2) / NP;
+        const int jgp = (loop_idx % NP2) % NP;
 
-        ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]> qdp   = Homme::subview(m_elements.m_qdp,kv.ie,m_data.qn0,iq);
-        ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>       q_buf = Homme::subview(m_elements.buffers.qtens,kv.ie,iq);
-        ExecViewUnmanaged<Scalar[2][NP][NP][NUM_LEV]>    v_buf = Homme::subview(m_elements.buffers.vstar_qdp,kv.ie,iq);
+        const ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]>
+          qdp   = Kokkos::subview(m_elements.m_qdp, kv.ie, m_data.qn0, iq, ALL, ALL, ALL);
+        const ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>
+          q_buf = Kokkos::subview(m_elements.buffers.qtens, kv.ie, iq, ALL, ALL, ALL);
+        const ExecViewUnmanaged<Scalar[2][NP][NP][NUM_LEV]>
+          v_buf = Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, iq, ALL, ALL, ALL, ALL);
 
-        Kokkos::parallel_for (
-          Kokkos::ThreadVectorRange (team, NP*NP),
-          [&] (const int idx)
-          {
-            const int igp = idx / NP;
-            const int jgp = idx % NP;
-
-            v_buf(0,igp,jgp,kv.ilev) = m_elements.buffers.vstar(kv.ie,0,igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
-            v_buf(1,igp,jgp,kv.ilev) = m_elements.buffers.vstar(kv.ie,1,igp,jgp,kv.ilev) * qdp(igp,jgp,kv.ilev);
-            q_buf(igp,jgp,kv.ilev) = qdp(igp,jgp,kv.ilev);
-          }
-        );
-
-        divergence_sphere_update(kv, -m_data.dt, 1.0, dinv, metdet,
-                                 m_deriv.get_dvv(), v_buf, q_buf);
+        for (int ilev = 0; ilev < NUM_LEV; ++ilev) {
+          v_buf(0,igp,jgp,ilev) = (m_elements.buffers.vstar(kv.ie, 0, igp, jgp, ilev) *
+                                   qdp(igp, jgp, ilev));
+          v_buf(1,igp,jgp,ilev) = (m_elements.buffers.vstar(kv.ie, 1, igp, jgp, ilev) *
+                                   qdp(igp, jgp, ilev));
+          q_buf(igp,jgp,ilev) = qdp(igp,jgp,ilev);
+        }
       }
     );
+    kv.team_barrier();
+
+    const auto dvv = m_deriv.get_dvv();
+    const ExecViewUnmanaged<const Real[2][2][NP][NP]>
+      dinv = Kokkos::subview(m_elements.m_dinv, kv.ie, ALL, ALL, ALL, ALL);
+    for (int iq = 0; iq < m_data.qsize; ++iq)
+      divergence_sphere_update(
+        kv, -m_data.dt, 1.0, dinv, metdet, dvv,
+        Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, iq, ALL, ALL, ALL, ALL),
+        m_elements.buffers.vdp,
+        Kokkos::subview(m_elements.buffers.qtens, kv.ie, iq, ALL, ALL, ALL));
   }
 
 };
