584f1f462cb17fd0d2ef6fbf40d0f41c591a2337
Andrew M. Bradley <ambradl@sandia.gov>
2017-10-30 11:13:35 -0600

    WIP EulerStepFunctor topic branch.
    This branch will focus on the on-node work in euler_step. Probably it will be
    merged into master when that is done. Then, once the C++ MPI branch is merged
    into master, a new topic branch will focus on switching to the C++ MPI code. A
    valuable conversation with Oksana and subsequent analysis shows the following
    must be done:
    
        prim_advection_mod_base.F90
        active defines: OVERLAP NEWEULER_B4B _PRIM
        Prim_Advec_Tracers_remap_rk2
          euler_step
            ! compute Q min/max values for lim8
            ! compute biharmonic mixing term f
            if rhs_multiplier = 0
              neighbor_minmax
            elif rhs_multiplier = 2
              neighbor_minmax_start
              biharmonic_wk_scalar
              neighbor_minmax_finish
            ! 2D Advection step
            advance_qdp
            limiter_optim_iter_full
            bndry_exchangeV
        viscosity_mod_base.F90
          neighbor_minmax*: S(calar) bndry exchange + trivial min/max
          biharmonic_wk_scalar
            laplace_sphere_wk
            bndry_exchangeV
            laplace_sphere_wk
        derivative_mod_base.F90
          limiter_optim_iter_full: the cell-local QP solver
    
    So far, this branch has implemented roughly the right parallel dispatch that
    we'll need, but it has hacks and to-dos related to reworking
    DefaultThreadsDistribution.

--- src/share/cxx/EulerStepFunctor.hpp
+++ src/share/cxx/EulerStepFunctor.hpp
@@ -11,9 +11,13 @@ namespace Homme
 
 struct EulerStepFunctor
 {
-  const Control     m_data;
+  Control           m_data;
   const Elements    m_elements;
   const Derivative  m_deriv;
+
+  struct TagVstar {};
+  struct TagDivUpdate {};
+  struct TagFused {};
 
   EulerStepFunctor (const Control& data)
    : m_data    (data)
@@ -30,48 +34,107 @@ struct EulerStepFunctor
   }
 
   KOKKOS_INLINE_FUNCTION
-  void operator() (const TeamMember& team) const {
-    KernelVariables kv(team);
+  void operator() (const TagVstar&, const TeamMember& team) const {
+    KernelVariables kv(team, m_data.qsize);
+    compute_vstar_qdp(kv);
+  }
 
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const TagDivUpdate&, const TeamMember& team) const {
+    KernelVariables kv(team, m_data.qsize);
+    compute_qtens(kv);
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  void operator() (const TagFused&, const TeamMember& team) const {
+    start_timer("esf compute");
+    KernelVariables kv(team, m_data.qsize);
+    compute_vstar_qdp(kv);
+    compute_qtens(kv);
+    stop_timer("esf compute");
+  }
+
+  static void run() {
+    // Get control structure
+    Control& data = get_control();
+
+    // Create the functor
+    EulerStepFunctor func(data);
+
+    profiling_resume();
+#if 1
+    Kokkos::parallel_for(get_policy<TagFused>(data), func);
+#else
+    Kokkos::parallel_for(get_policy<TagVstar>(data), func);
+    Kokkos::fence();
+    Kokkos::parallel_for(get_policy<TagDivUpdate>(data), func);
+#endif
+
+    // Finalize
+    ExecSpace::fence();
+    profiling_pause();
+  }
+
+private:
+
+  template <typename Tag>
+  static Kokkos::TeamPolicy<ExecSpace, Tag> get_policy(const Control& data) {
+    const int vectors_per_thread =
+      DefaultThreadsDistribution<ExecSpace>::vectors_per_thread();
+    //todo Need to rework threading setup.
+    const int threads_per_team   =
+      std::is_same<ExecSpace,Hommexx_Cuda>::value ?
+      DefaultThreadsDistribution<ExecSpace>::threads_per_team(1) :
+      std::max(1, (DefaultThreadsDistribution<ExecSpace>::threads_per_team(data.num_elems) /
+                   data.qsize));
+    Kokkos::TeamPolicy<ExecSpace, Tag> policy(data.num_elems * data.qsize,
+                                              threads_per_team,
+                                              vectors_per_thread);
+    policy.set_chunk_size(1);
+    return policy;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  void compute_vstar_qdp (const KernelVariables& kv) const {
     using Kokkos::ALL;
-    const ExecViewUnmanaged<const Real[NP][NP]>
-      metdet = Kokkos::subview(m_elements.m_metdet, kv.ie, ALL, ALL);
-
     const auto NP2 = NP * NP;
     Kokkos::parallel_for (
-      Kokkos::TeamThreadRange(team, NP2*m_data.qsize),
+      Kokkos::TeamThreadRange(kv.team, NP2),
       [&] (const int loop_idx) {
-        const int iq  = loop_idx / NP2;
-        const int igp = (loop_idx % NP2) / NP;
-        const int jgp = (loop_idx % NP2) % NP;
+        const int igp = loop_idx / NP;
+        const int jgp = loop_idx % NP;
 
         const ExecViewUnmanaged<const Scalar[NP][NP][NUM_LEV]>
-          qdp   = Kokkos::subview(m_elements.m_qdp, kv.ie, m_data.qn0, iq, ALL, ALL, ALL);
+          qdp   = Kokkos::subview(m_elements.m_qdp, kv.ie, m_data.qn0, kv.iq, ALL, ALL, ALL);
         const ExecViewUnmanaged<Scalar[NP][NP][NUM_LEV]>
-          q_buf = Kokkos::subview(m_elements.buffers.qtens, kv.ie, iq, ALL, ALL, ALL);
+          q_buf = Kokkos::subview(m_elements.buffers.qtens, kv.ie, kv.iq, ALL, ALL, ALL);
         const ExecViewUnmanaged<Scalar[2][NP][NP][NUM_LEV]>
-          v_buf = Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, iq, ALL, ALL, ALL, ALL);
+          v_buf = Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, kv.iq, ALL, ALL, ALL, ALL);
 
-        for (int ilev = 0; ilev < NUM_LEV; ++ilev) {
+        Kokkos::parallel_for(Kokkos::ThreadVectorRange(kv.team, NUM_LEV), [&] (const int& ilev) {
           v_buf(0,igp,jgp,ilev) = (m_elements.buffers.vstar(kv.ie, 0, igp, jgp, ilev) *
                                    qdp(igp, jgp, ilev));
           v_buf(1,igp,jgp,ilev) = (m_elements.buffers.vstar(kv.ie, 1, igp, jgp, ilev) *
                                    qdp(igp, jgp, ilev));
           q_buf(igp,jgp,ilev) = qdp(igp,jgp,ilev);
-        }
+        });
       }
     );
-    kv.team_barrier();
+  }
 
+  KOKKOS_INLINE_FUNCTION
+  void compute_qtens (const KernelVariables& kv) const {
+    using Kokkos::ALL;
     const auto dvv = m_deriv.get_dvv();
+    const ExecViewUnmanaged<const Real[NP][NP]>
+      metdet = Kokkos::subview(m_elements.m_metdet, kv.ie, ALL, ALL);
     const ExecViewUnmanaged<const Real[2][2][NP][NP]>
       dinv = Kokkos::subview(m_elements.m_dinv, kv.ie, ALL, ALL, ALL, ALL);
-    for (int iq = 0; iq < m_data.qsize; ++iq)
-      divergence_sphere_update(
-        kv, -m_data.dt, 1.0, dinv, metdet, dvv,
-        Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, iq, ALL, ALL, ALL, ALL),
-        m_elements.buffers.vdp,
-        Kokkos::subview(m_elements.buffers.qtens, kv.ie, iq, ALL, ALL, ALL));
+    divergence_sphere_update(
+      kv, -m_data.dt, 1.0, dinv, metdet, dvv,
+      Kokkos::subview(m_elements.buffers.vstar_qdp, kv.ie, kv.iq, ALL, ALL, ALL, ALL),
+      m_elements.buffers.vdp,
+      Kokkos::subview(m_elements.buffers.qtens, kv.ie, kv.iq, ALL, ALL, ALL));
   }
 
 };
