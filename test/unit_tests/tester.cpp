aeadfd068e8da4dbf358ccd5f99b63c9e77db0f5
Luca Bertagna <lbertag@sandia.gov>
2017-10-31 09:28:00 -0600

    Small modification to tester.cpp, to allow MPI executions

--- test/unit_tests/tester.cpp
+++ test/unit_tests/tester.cpp
@@ -6,14 +6,27 @@
 #include <Kokkos_Core.hpp>
 
 #include "Hommexx_Session.hpp"
+#ifdef HOMMEXX_MPI_TEST
+#include <mpi.h>
+#endif
 
 int main(int argc, char **argv) {
 
   Homme::initialize_hommexx_session();
 
+#ifdef HOMMEXX_MPI_TEST
+  // Initialize mpi
+  MPI_Init(&argc,&argv);
+#endif
+
   int result = Catch::Session().run(argc, argv);
 
   Homme::finalize_hommexx_session();
+
+#ifdef HOMMEXX_MPI_TEST
+  // Finalize mpi
+  MPI_Finalize();
+#endif
 
   return result;
 }
