60cdceaccf4e0bc9373695f8f511d4bf4e154960
Luca Bertagna <lbertag@sandia.gov>
2017-11-01 16:06:38 -0600

    Removed replaced caar_pre_exchange_monolithic_c with caar_monolithic_c.
    The Fortran-callable routine now handles the whole compute_and_apply_rhs
    subroutine (including MPI).

--- components/homme/src/share/cxx/cxx_f90_interface.cpp
+++ components/homme/src/share/cxx/cxx_f90_interface.cpp
@@ -5,6 +5,9 @@
 
 #include "CaarFunctor.hpp"
 #include "EulerStepFunctor.hpp"
+#include "BoundaryExchange.hpp"
+
+#include "Utility.hpp"
 
 #include "profiling.hpp"
 
@@ -18,7 +21,7 @@ void init_control_caar_c (const int& nets, const int& nete, const int& num_elems
                           const int& nm1, const int& n0, const int& np1,
                           const int& qn0, const Real& dt2, const Real& ps0,
                           const bool& compute_diagonstics, const Real& eta_ave_w,
-                          CRCPtr& hybrid_a_ptr)
+                          const int& rsplit, CRCPtr& hybrid_a_ptr)
 {
   Control& control = Context::singleton().get_control ();
 
@@ -30,6 +33,8 @@ void init_control_caar_c (const int& nets, const int& nete, const int& num_elems
   int qn0_c = qn0==-1 ? qn0 : qn0-1;  // the -1 index has a special meaning, and -2 is not even contemplated
   int np1_c = np1-1;
   control.init(nets_c, nete_c, num_elems, nm1_c, n0_c, np1_c, qn0_c, dt2, ps0, compute_diagonstics, eta_ave_w, hybrid_a_ptr);
+
+  control.rsplit = rsplit;
 }
 
 void init_control_euler_c (const int& nets, const int& nete, const int& qn0, const int& qsize, const Real& dt)
@@ -54,11 +59,11 @@ void init_derivative_c (CF90Ptr& dvv)
 }
 
 void init_elements_2d_c (const int& num_elems, CF90Ptr& D, CF90Ptr& Dinv, CF90Ptr& fcor,
-                       CF90Ptr& spheremp, CF90Ptr& metdet, CF90Ptr& phis)
+                         CF90Ptr& spheremp, CF90Ptr& rspheremp, CF90Ptr& metdet, CF90Ptr& phis)
 {
   Elements& r = Context::singleton().get_elements ();
   r.init (num_elems);
-  r.init_2d(D,Dinv,fcor,spheremp,metdet,phis);
+  r.init_2d(D,Dinv,fcor,spheremp,rspheremp,metdet,phis);
 }
 
 void caar_pull_data_c (CF90Ptr& elem_state_v_ptr, CF90Ptr& elem_state_t_ptr, CF90Ptr& elem_state_dp3d_ptr,
@@ -139,28 +144,69 @@ void euler_push_results_c (F90Ptr& qtens_ptr)
   }
 }
 
-void caar_pre_exchange_monolithic_c()
+void caar_monolithic_c()
 {
-  // Get control structure
+  // Get control and elements structures
   Control& data  = Context::singleton().get_control();
+  Elements& elements = Context::singleton().get_elements();
 
   // Retrieve the team size
   const int vectors_per_thread = DefaultThreadsDistribution<ExecSpace>::vectors_per_thread();
   const int threads_per_team   = data.team_size;
 
-  // Setup the policy
-  Kokkos::TeamPolicy<ExecSpace> policy(data.num_elems, threads_per_team, vectors_per_thread);
-  policy.set_chunk_size(1);
-
   // Create the functor
-  CaarFunctor func(data, Context::singleton().get_elements(),
-                   Context::singleton().get_derivative());
+  CaarFunctor func(data);
 
+  // Setup the policies
+  Kokkos::TeamPolicy<ExecSpace,CaarFunctor::TagPreExchange>  policy_pre  (data.num_elems, threads_per_team, vectors_per_thread);
+  Kokkos::TeamPolicy<ExecSpace,CaarFunctor::TagPostExchange> policy_post (data.num_elems, threads_per_team, vectors_per_thread);
+  policy_pre.set_chunk_size(1);
+  policy_post.set_chunk_size(1);
+
+  // --- Pre boundary exchange
   profiling_resume();
-  // Dispatch parallel for
-  Kokkos::parallel_for("main caar loop", policy, func);
+  Kokkos::parallel_for("caar loop pre-boundary exchange", policy_pre, func);
+  ExecSpace::fence();
+  profiling_pause();
 
-  // Finalize
+  // Do the boundary exchange
+  BoundaryExchange* exchanges[NUM_TIME_LEVELS];
+  for (int tl=0; tl<NUM_TIME_LEVELS; ++tl) {
+    std::stringstream ss;
+    ss << "caar tl " << tl;
+    exchanges[tl] = &get_boundary_exchange(ss.str());
+  }
+
+  BoundaryExchange& be = *exchanges[data.np1];
+
+  if (!be.is_registration_completed())
+  {
+    be.set_num_fields(0,4);
+    be.register_field(elements.h_u,1,data.np1);
+    be.register_field(elements.h_v,1,data.np1);
+    be.register_field(elements.h_t,1,data.np1);
+    be.register_field(elements.h_dp3d,1,data.np1);
+
+    be.registration_completed();
+  }
+
+  // Copy to host
+  Kokkos::deep_copy(elements.h_u,elements.m_u);
+  Kokkos::deep_copy(elements.h_v,elements.m_v);
+  Kokkos::deep_copy(elements.h_t,elements.m_t);
+  Kokkos::deep_copy(elements.h_dp3d,elements.m_dp3d);
+
+  be.exchange();
+
+  // Copy to device
+  Kokkos::deep_copy(elements.m_u,elements.h_u);
+  Kokkos::deep_copy(elements.m_v,elements.h_v);
+  Kokkos::deep_copy(elements.m_t,elements.h_t);
+  Kokkos::deep_copy(elements.m_dp3d,elements.h_dp3d);
+
+  // --- Post boundary echange
+  profiling_resume();
+  Kokkos::parallel_for("caar loop post-boundary exchange", policy_post, func);
   ExecSpace::fence();
   profiling_pause();
 }
