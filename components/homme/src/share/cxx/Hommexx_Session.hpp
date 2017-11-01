0060f70698ad7d3f68eaac44d5775aebc4475692
Luca Bertagna <lbertag@sandia.gov>
2017-11-01 17:04:29 -0600

    A couple of routines to declare the beginning and the end of a hommexx epoch.
    These routines can/should handl all stuff that needs to be initialized
    at the beginning of the execution to allow C++ code to run fine, and also
    handle the finalization of all C++ structures before the program terminates.
    
    Notably, they initialize/finalize Kokkos, and make sure that the structures
    containing Kokkos views inside the Context singleton are cleaned up before
    kokkos is finalized.

---
+++ components/homme/src/share/cxx/Hommexx_Session.hpp
@@ -0,0 +1,16 @@
+#ifndef HOMMEXX_SESSION_HPP
+#define HOMMEXX_SESSION_HPP
+
+namespace Homme
+{
+
+extern "C" {
+
+void initialize_hommexx_session ();
+void finalize_hommexx_session ();
+
+} // extern "C"
+
+} // namespace Homme
+
+#endif // HOMMEXX_SESSION_HPP
