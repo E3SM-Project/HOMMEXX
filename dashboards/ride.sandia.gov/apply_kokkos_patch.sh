e79c5424aca5be9d56b6c1eb235df124b2ae831b
Irina K. Tezaur <ikalash@sandia.gov>
2017-11-01 15:24:56 -0700

    Edits to Ride nightly test scripts.

--- dashboards/ride.sandia.gov/apply_kokkos_patch.sh
+++ dashboards/ride.sandia.gov/apply_kokkos_patch.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-cd ${WORKSPACE}/build
+cd /home/projects/hommexx/nightlyCDash/build
 mkdir KokkosBuild
 cd KokkosBuild 
-${jenkins_trilinos_dir}/packages/kokkos/generate_makefile.bash --prefix=${WORKSPACE}/build/TrilinosInstall --with-openmp --with-serial --with-cuda=${CUDA_ROOT} --with-cuda-options=enable_lambda 
+${jenkins_trilinos_dir}/packages/kokkos/generate_makefile.bash --prefix=/home/projects/hommexx/nightlyCDash/build/TrilinosInstall --with-openmp --with-serial --with-cuda=${CUDA_ROOT} --with-cuda-options=enable_lambda 
 make -j 24
 make install -j 24 
--- dashboards/ride.sandia.gov/nightly_cron_script_hommexx_ride_cuda.sh
+++ dashboards/ride.sandia.gov/nightly_cron_script_hommexx_ride_cuda.sh
@@ -1,6 +1,6 @@
 #!/bin/csh
 
-BASE_DIR=${WORKSPACE}
+BASE_DIR=/home/projects/hommexx/nightlyCDash
 cd $BASE_DIR
 
 unset http_proxy
