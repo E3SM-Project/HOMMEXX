e2943a3962835a2d21cc3d5476d4d1424db9f39c
Luca Bertagna <lbertag@sandia.gov>
2017-11-01 17:04:29 -0600

    A couple of routines to declare the beginning and the end of a hommexx epoch.
    These routines can/should handl all stuff that needs to be initialized
    at the beginning of the execution to allow C++ code to run fine, and also
    handle the finalization of all C++ structures before the program terminates.
    
    Notably, they initialize/finalize Kokkos, and make sure that the structures
    containing Kokkos views inside the Context singleton are cleaned up before
    kokkos is finalized.

---
+++ src/share/cxx/Hommexx_Session.cpp
@@ -0,0 +1,34 @@
+#include "Hommexx_Session.hpp"
+
+#include <ExecSpaceDefs.hpp>
+#include "profiling.hpp"
+#include "Context.hpp"
+
+#include <iostream>
+
+namespace Homme
+{
+
+void initialize_hommexx_session ()
+{
+  /* Make certain profiling is only done for code we're working on */
+  profiling_pause();
+
+  /* Set Environment variables to control how many
+   * threads/processors Kokkos uses */
+  Kokkos::initialize();
+
+  ExecSpace::print_configuration(std::cout, true);
+
+  // Put here other initialization routines (e.g., MPI)
+}
+
+void finalize_hommexx_session ()
+{
+  Context::finalize_singleton();
+  Kokkos::finalize();
+
+  // Put here other finalization routines (e.g., MPI)
+}
+
+} // namespace Homme
