e0b6916a9584c77849e167f646cc43240e9d0336
onguba <oksanaguba@users.noreply.github.com>
2017-11-02 12:46:13 -0600

    Adding moist tests. (#104)
    * Adding short moist tests (testing is next).
    
    * A few tweeaks to have names like ...moist-short-f(c), so that checks F vs C work (checks rely on suffixes -f and -c to be last). This commit was run for \make baseline\ and \make check\ and everything looks like it should. I also manually verified that moist tests (except for r0 and -c) have 2 frames in *nc files and did not blow up.

---
+++ test/reg_test/run_tests/prtcB-r3-moist-short-f.cmake
@@ -0,0 +1,16 @@
+
+# The name of this test (should be the basename of this file)
+SET(TEST_NAME prtcB-r3-moist-short-f)
+# The specifically compiled executable that this test uses
+SET(EXEC_NAME prtcB)
+
+SET(NUM_CPUS 16)
+
+SET(NAMELIST_FILES ${HOMME_ROOT}/test/reg_test/namelists/prtcB-r3-moist-short.nl)
+SET(VCOORD_FILES ${HOMME_ROOT}/test/vcoord/acme-72*)
+
+# compare all of these files against baselines:
+SET(NC_OUTPUT_FILES
+  jw_baroclinic1.nc
+  jw_baroclinic2.nc)
+
