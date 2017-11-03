825f306e85e8396cee6732492740703bd74beefe
Irina K. Tezaur <ikalash@sandia.gov>
2017-11-03 12:14:58 -0700

    More tweaks and cleanups to Ride nightly dashboard scripts.

--- dashboards/ride.sandia.gov/run_all_lsf.sh
+++ dashboards/ride.sandia.gov/run_all_lsf.sh
@@ -12,7 +12,7 @@ rm -rf *err
 ulimit -c 0
 
 #bash -c -l "source ride_modules_cuda.sh >& modules_trilinos.out; bash nightly_cron_script_trilinos_ride_cuda.sh"
-bash -c -l "source ride_modules_cuda.sh >& modules_trilinos.out; bash apply_kokkos_patch.sh >& apply_kokkos_patch.out"
+bash -c -l "source ride_modules_cuda.sh >& modules_trilinos.out; bash build_kokkos.sh >& build_kokkos.out"
 bash -c -l "source ride_modules_cuda.sh >& modules_hommexx.out; bash nightly_cron_script_hommexx_ride_cuda.sh"
 bash process_results_ctest_cuda.sh
 bash send_email_ctest_cuda.sh
