a6f4f68bf5f62f044d33f6daa111dc2fabfa96b0
Irina K. Tezaur <ikalash@sandia.gov>
2017-11-02 21:33:39 -0600

    Updating skybridge module for netcdf.

--- components/homme/dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
+++ components/homme/dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
@@ -2,5 +2,6 @@ source /projects/sems/modulefiles/utils/sems-modules-init.sh
 module load intel/16.0
 module load openmpi-intel/1.10
 module load sems-env 
-module add sems-hdf5/1.8.12/base sems-netcdf/4.3.2/base
+module add sems-hdf5/1.8.12/base sems-netcdf/4.4.1/exo
 module list 
+export OMP_NUM_THREADS=4
