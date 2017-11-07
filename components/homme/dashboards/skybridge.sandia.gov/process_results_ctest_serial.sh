055fe444feb8c48c3fd66d0541915bd5b80c1d44
Irina K. Tezaur <ikalash@sandia.gov>
2017-11-06 19:58:14 -0800

    Modifying skybridge nightly tests.  Includes some cleanups and moving nightly builds to newly created /projects/hommexx directory.

--- components/homme/dashboards/skybridge.sandia.gov/process_results_ctest_serial.sh
+++ components/homme/dashboards/skybridge.sandia.gov/process_results_ctest_serial.sh
@@ -1,5 +1,5 @@
 
-cd /home/ikalash/nightlyHOMMEXXCDash
+cd /projects/hommexx/nightlyHOMMEXXCDash
 grep "Test   #" nightly_log_skybridgeHOMMEXXserial.txt >& results0
 grep "Test  #" nightly_log_skybridgeHOMMEXXserial.txt >& results1
 cat results0 results1 >& results11
@@ -13,6 +13,8 @@ cat results3 results4 >& results5
 echo "" >> results5
 grep "...   Passed" nightly_log_skybridgeHOMMEXXserial.txt >& results6
 echo "The HOMMEXX CDash site can be accessed here: http://cdash.sandia.gov/CDash-2-3-0/index.php?project=HOMMEXX (SRN)." >> results5
+echo "" >> results5
+echo "The nightly build can be found on Skybridge in the directory: /projects/hommexx/nightlyHOMMEXXCDash." >> results5
 echo "" >> results5
 mv results5 results_hommexx_serial 
 rm results0 results1 results11 results2 results3 results4 results6
--- components/homme/dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
+++ components/homme/dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
@@ -2,10 +2,10 @@
 
 #source $1 
 
-TTT=`grep "(Failed)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
-TTTT=`grep "(Not Run)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
-TTTTT=`grep "(Timeout)" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
-TT=`grep "...   Passed" /home/ikalash/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
+TTT=`grep "(Failed)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
+TTTT=`grep "(Not Run)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
+TTTTT=`grep "(Timeout)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
+TT=`grep "...   Passed" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
 
-/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /home/ikalash/nightlyHOMMEXXCDash/results_hommexx_openmp
+/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /projects/hommexx/nightlyHOMMEXXCDash/results_hommexx_openmp
 
