426a22cb3b0d7ee885844a89598b156349d7c3dc
Luca Bertagna <lbertag@sandia.gov>
2017-11-13 12:41:40 -0700

    Some changes in Connectivity class
    The class stores a 2D view now, with 8 ConnectionInfo per element. Notice
    that some of these may be MISSING, in case the element is one of the 24 that
    touch one of the cube vertices.
    
    Furthermore, I changed the ConnectionInfo and LidPos behavior. In particular,
    we no longer store the 'type' of a connection (the possible types used to be
    CORNER, EDGE_CONTIGUOUS_FWD, EDGE_CONTIGUOUS_BWD and EDGE_STRIDED). LidPos
    does not store the position as a number [0,4), but as [0,8). In other words,
    we no longer make a distinction between edge and corner, at this level. The
    constexpr array CONNECTION_SIZE is used to assess how many points are on a
    particular conneciton kind.

--- src/share/cxx/mpi/Connectivity.cpp
+++ src/share/cxx/mpi/Connectivity.cpp
@@ -5,53 +5,38 @@
 namespace Homme
 {
 
-bool operator<(const ConnectionInfo& a, const ConnectionInfo& b)
-{
-  if (a.remote_pid==-1 && b.remote_pid>=0) {
-    // Local connections come first, then shared
-    return true;
-  } else if (b.remote_pid==-1 && a.remote_pid>=0) {
-    // Local connections come first, then shared
-    return false;
-  } else if (a.kind!=b.kind) {
-    // If both local or both shared with the same pid, put corner connections first
-    return a.kind==CORNER_KIND;
-  } else if (a.local.lid!=b.local.lid){
-    // Within corner/edges, order by local element lid
-    return a.local.lid<b.local.lid;
-  } else {
-    // If same connection type within the same element, oder by neighbor position
-    return a.local.pos<b.local.pos;
-  }
-}
-
 Connectivity::Connectivity ()
  : m_finalized (false)
- , m_num_my_elems (-1)
+ , m_num_elements (-1)
 {
   // TODO: change this to allow integration of Hommexx in acme, and
   //       pass the number of processes dedicated to homme
   m_comm.init();
 }
 
-void Connectivity::set_num_my_elems (const int num_my_elems)
+void Connectivity::set_num_elements (const int num_elements)
 {
-  m_num_my_elems = num_my_elems;
-}
+  m_num_elements = num_elements;
 
-void Connectivity::set_num_connections (const int num_local_connections, const int num_shared_connections)
-{
-  m_connections.resize(num_local_connections+num_shared_connections);
+  m_connections = HostViewManaged<ConnectionInfo*[NUM_CONNECTIONS]>("Connections", m_num_elements);
+  m_num_connections = HostViewManaged<int[NUM_CONNECTION_SHARINGS+1][NUM_CONNECTION_KINDS+1]>("Connections counts");
 
-  // We do not set these two. We will use them as progressing index, updating them as we add connections.
-  // When finalize() is called, we will set them to match the size of the corresponding vector.
-  m_num_connections = 0;
-  m_num_local_connections  = 0;
-  m_num_shared_connections = 0;
-  m_num_local_corner_connections  = 0;
-  m_num_local_edge_connections    = 0;
-  m_num_shared_corner_connections = 0;
-  m_num_shared_edge_connections   = 0;
+  Kokkos::deep_copy (m_num_connections,0);
+
+  // Initialize all connections to MISSING
+  // Note: we still include local element/position, since we need that even for
+  //       missinc connections!
+  for (int ie=0; ie<m_num_elements; ++ie) {
+    for (int iconn=0; iconn<NUM_CONNECTIONS; ++iconn) {
+      ConnectionInfo& info = m_connections(ie,iconn);
+
+      info.sharing = etoi(ConnectionSharing::MISSING);
+      info.kind    = etoi(ConnectionKind::MISSING);
+
+      info.local.lid = ie;
+      info.local.pos = iconn;
+    }
+  }
 }
 
 void Connectivity::add_connection (const int first_elem_lid,  const int first_elem_pos,  const int first_elem_pid,
@@ -68,99 +53,76 @@ void Connectivity::add_connection (const int first_elem_lid,  const int first_el
   if (first_elem_pid==m_comm.m_rank)
   {
 #ifdef HOMMEXX_DEBUG
-    // Check we are not adding more connections than we promised
-    assert (static_cast<size_t>(m_num_connections)<m_connections.size());
-
     // There is no edge-to-corner connection. Either the elements share a corner or an edge.
-    assert (IS_EDGE_NEIGHBOR[first_elem_pos]==IS_EDGE_NEIGHBOR[second_elem_pos]);
+    assert (CONNECTION_KIND[first_elem_pos]==CONNECTION_KIND[second_elem_pos]);
 #endif
 
-    // Recall: corner=0, edge_contiguous_fwd=1, edge_contiguous_bwd=2, edge_strided_fwd=3, edge_strided_bwd=4
-    int is_edge    = IS_EDGE_NEIGHBOR[first_elem_pos];
-    int direction  = is_edge==1 ? NEIGHBOR_EDGE_DIRECTION[first_elem_pos][second_elem_pos] : 0;
-
-    ConnectionInfo& info = m_connections[m_num_connections];
-    LidPosType& local  = info.local;
-    LidPosType& remote = info.remote;
+    ConnectionInfo& info = m_connections(first_elem_lid,first_elem_pos);
+    LidPos& local  = info.local;
+    LidPos& remote = info.remote;
 
     // Local and remote elements lid and connection position
     local.lid  = first_elem_lid;
-    local.pos  = first_elem_pos % NUM_EDGES;
+    local.pos  = first_elem_pos;
     remote.lid = second_elem_lid;
-    remote.pos = second_elem_pos % NUM_EDGES;
+    remote.pos = second_elem_pos;
 
     // Kind
-    info.kind = is_edge==1 ? EDGE_KIND : CORNER_KIND;
+    info.kind = etoi(CONNECTION_KIND[first_elem_pos]);
 
-    // Direction and remote element pid (set to -1 for now)
-    info.remote_pid = -1;
-    info.direction  = direction;
+
+    // Direction
+    info.direction  = etoi(CONNECTION_DIRECTION[local.pos][remote.pos]);
 
     if (second_elem_pid!=m_comm.m_rank)
     {
-      // It is an connection between different processes. We need some extra info
-      // Local and remote connection type
-      local.type = info.kind==CORNER_KIND ?
-                     CORNER :
-                     (IS_STRIDED_EDGE[local.pos] ?
-                        EDGE_STRIDED:
-                        EDGE_CONTIGUOUS_FWD);
-
-      remote.type = info.kind==CORNER_KIND ?
-                      CORNER :
-                        (direction==DIRECTION_FWD ? EDGE_CONTIGUOUS_FWD : EDGE_CONTIGUOUS_BWD);
-
-      // Remote element pid
+      info.sharing = etoi(ConnectionSharing::SHARED);
       info.remote_pid = second_elem_pid;
 
+    } else {
+      info.remote_pid = -1;
+      info.sharing = etoi(ConnectionSharing::LOCAL);
     }
-    ++m_num_connections;
+
+    ++m_num_connections(info.sharing,info.kind);
   }
 }
 
 void Connectivity::finalize()
 {
-  // We sort the connections, so we can easily subview them by type (shared/local and corner/edge)
-  std::sort(m_connections.begin(),m_connections.end());
+  // Sanity check: Homme does not allow less than 2*2 elements per face, so each element
+  // should have at most ONE missing connection
+  for (int ie=0; ie<m_num_elements; ++ie) {
+    bool missing[NUM_CORNERS] = {false, false, false, false};
 
-  // Count local/shared connections and, within local/shared, count corner/edge connections
-  for (const ConnectionInfo& info : m_connections) {
-    if (info.remote_pid==-1) {
-      ++(info.kind==CORNER_KIND ? m_num_local_corner_connections : m_num_local_edge_connections);
-      ++m_num_local_connections;
-    } else {
-      ++(info.kind==CORNER_KIND ? m_num_shared_corner_connections : m_num_shared_edge_connections);
-      ++m_num_shared_connections;
+    for (int ic=0; ic<NUM_CORNERS; ++ic) {
+      if (m_connections(ie,ic+CORNERS_OFFSET).kind == etoi(ConnectionKind::MISSING)) {
+        missing[ic] = true;
+
+        // Just for tracking purposes
+        ++m_num_connections(etoi(ConnectionSharing::MISSING),etoi(ConnectionKind::MISSING));
+      }
     }
+    assert (std::count(missing,missing+NUM_CORNERS,true)<=1);
   }
 
-  m_local_connections  = HostViewUnmanaged<const ConnectionInfo*>(m_connections.data(),m_num_local_connections);
-  m_shared_connections = HostViewUnmanaged<const ConnectionInfo*>(m_connections.data()+m_num_local_connections,m_num_shared_connections);
-
-  m_local_corner_connections  = HostViewUnmanaged<const ConnectionInfo*>(m_local_connections.data(),m_num_local_corner_connections);
-  m_local_edge_connections    = HostViewUnmanaged<const ConnectionInfo*>(m_local_connections.data()+m_num_local_corner_connections,m_num_local_edge_connections);
-  m_shared_corner_connections = HostViewUnmanaged<const ConnectionInfo*>(m_shared_connections.data(),m_num_shared_corner_connections);
-  m_shared_edge_connections   = HostViewUnmanaged<const ConnectionInfo*>(m_shared_connections.data()+m_num_shared_corner_connections,m_num_shared_edge_connections);
+  // Updating counters for groups with same sharing/kind
+  for (int kind=0; kind<NUM_CONNECTION_KINDS; ++kind) {
+    for (int sharing=0; sharing<NUM_CONNECTION_SHARINGS; ++sharing) {
+      m_num_connections(etoi(ConnectionSharing::ANY),kind) += m_num_connections(sharing,kind);
+      m_num_connections(sharing,etoi(ConnectionKind::ANY)) += m_num_connections(sharing,kind);
+    }
+    m_num_connections(etoi(ConnectionKind::ANY),etoi(ConnectionKind::ANY)) += m_num_connections(etoi(ConnectionSharing::ANY),kind);
+  }
 
   m_finalized = true;
 }
 
 void Connectivity::clean_up()
 {
-  m_local_corner_connections  = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
-  m_local_edge_connections    = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
-  m_shared_corner_connections = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
-  m_shared_edge_connections   = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
+  m_connections = HostViewManaged<ConnectionInfo*[NUM_CONNECTIONS]>("",0);
 
-  m_local_connections  = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
-  m_shared_connections = HostViewUnmanaged<const ConnectionInfo*>(nullptr,0);
-
-  m_connections.resize(0);
-
-  m_num_connections = 0;
-  m_num_local_connections = m_num_shared_connections = 0;
-  m_num_local_edge_connections = m_num_local_corner_connections = 0;
-  m_num_shared_edge_connections = m_num_shared_corner_connections = 0;
+  Kokkos::deep_copy(m_num_connections,0);
 
   m_finalized = false;
 }
