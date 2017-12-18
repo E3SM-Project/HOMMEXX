971bf0c3231e1bf5683ab5a9b7ea70193de4db84
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-18 14:36:12 -0800

    Checking in up-to-date nighly test scripts for bowman/ellis.
    These are run on bowman currently but should work for ellis too.

---
+++ dashboards/bowman.sandia.gov/ellis_openmp_env.sh
@@ -0,0 +1,5 @@
+
+export OMP_NUM_THREADS=4
+export KMP_AFFINITY=balanced
+env | grep OMP_
+
--- dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
+++ dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOM
 TTTTT=`grep "(Timeout)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 
-mail -s " (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /home/projects/hommexx/nightlyCDash/results_hommexx
+mail -s " (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /home/projects/hommexx/nightlyCDash/results_hommexx
 
