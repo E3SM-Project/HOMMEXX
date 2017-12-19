dfe5b488bfbcefba57ec2add94dc4404bd82755b
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-18 17:01:01 -0800

    Updating email and cronjob scripts on Bowman.

--- components/homme/dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
+++ components/homme/dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
@@ -7,5 +7,12 @@ TTTT=`grep "(Not Run)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOM
 TTTTT=`grep "(Timeout)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 
-mail -s " (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /home/projects/hommexx/nightlyCDash/results_hommexx
+#echo -e "Subject: HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" | cat - /home/projects/hommexx/nightlyCDash/results_hommexx | /usr/lib/sendmail -F ikalash@bowman.sandia.gov -t ikalash@sandia.gov
 
+echo "Subject: HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
+echo "" >& b 
+cat a b >& c 
+cat c results_hommexx >& d
+mv d results_hommexx
+rm a b c 
+cat results_hommexx | /usr/lib/sendmail -F ikalash@bowman.sandia.gov -t ikalash@sandia.gov
