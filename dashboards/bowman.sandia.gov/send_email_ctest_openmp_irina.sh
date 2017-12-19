6a321dc820dc369b769541f9ce8731737e80e0cf
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-19 11:36:05 -0800

    Some mods to Bowman nightly CDash scripts.

--- dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
+++ dashboards/bowman.sandia.gov/send_email_ctest_openmp_irina.sh
@@ -7,9 +7,7 @@ TTTT=`grep "(Not Run)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOM
 TTTTT=`grep "(Timeout)" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /home/projects/hommexx/nightlyCDash/nightly_log_bowmanHOMMEXXopenmp.txt -c`
 
-#echo -e "Subject: HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" | cat - /home/projects/hommexx/nightlyCDash/results_hommexx | /usr/lib/sendmail -F ikalash@bowman.sandia.gov -t ikalash@sandia.gov
-
-echo "Subject: HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
+echo "Subject: HOMMEXX (master, OpenMP KokkosNode, KNL): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
 echo "" >& b 
 cat a b >& c 
 cat c results_hommexx >& d
