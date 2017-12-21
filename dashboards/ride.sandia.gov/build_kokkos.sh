24b5953627204c4678197badf987c75a0ec3cfd0
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-21 11:24:45 -0800

    Updating Ride Kokkos build script.

--- dashboards/ride.sandia.gov/build_kokkos.sh
+++ dashboards/ride.sandia.gov/build_kokkos.sh
@@ -7,6 +7,6 @@ cd build
 mkdir KokkosBuild
 cd KokkosBuild
 sed s/default_arch=\"sm_35\"/default_arch=\"sm_60\"/ -i ${jenkins_trilinos_dir}/packages/kokkos/config/nvcc_wrapper 
-${jenkins_trilinos_dir}/packages/kokkos/generate_makefile.bash --prefix=/home/projects/hommexx/nightlyCDash/build/KokkosInstall --with-serial --with-cuda=${CUDA_ROOT} --with-cuda-options=enable_lambda --with-options=aggressive_vectorization --arch=Pascal60
+${jenkins_trilinos_dir}/packages/kokkos/generate_makefile.bash --compiler=${jenkins_trilinos_dir}/packages/kokkos/config/nvcc_wrapper --prefix=/home/projects/hommexx/nightlyCDash/build/KokkosInstall --with-serial --with-cuda=${CUDA_ROOT} --with-cuda-options=enable_lambda --with-options=aggressive_vectorization --arch=Pascal60
 make -j 24
 make install -j 24 
