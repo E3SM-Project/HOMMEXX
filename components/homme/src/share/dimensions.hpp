52b59e4e0c5844c9ede3d30e7c69369846af98a7
Andrew M. Bradley <ambradl@sandia.gov>
2018-01-03 15:18:38 -0700

    Restore unused file dimensions.hpp to original state.
    This will be removed at some point. But it's orthogonal to this PR, so restore
    it so the PR is clean.

--- components/homme/src/share/dimensions.hpp
+++ components/homme/src/share/dimensions.hpp
@@ -1,4 +1,3 @@
-#ifndef HOMMEXX_DIMENSIONS_HPP
 #define HOMMEXX_DIMENSIONS_HPP
 
 #ifdef HAVE_CONFIG_H
@@ -7,6 +6,25 @@
 #include "Hommexx_config.h"
 
 namespace Homme {
+
+// Until whenever CUDA supports constexpr properly
+#ifdef HOMMEXX_CUDA_SPACE
+
+#define np                      NP
+#define npsq                    (np * np)
+#define max_neigh_edges         8
+#define nc                      NC
+#define dim                     2
+#ifdef QSIZE_D
+#define qsize_d                 QSIZE_D
+#else
+#define qsize_d                 4
+#endif
+#define nlev                    PLEV
+#define nlevp                   (nlev + 1)
+#define timelevels              3
+
+#else // HOMMEXX_CUDA_BUILD
 
 constexpr int np              = NP;
 constexpr int npsq            = np * np;
@@ -21,6 +39,8 @@ constexpr int qsize_d         = 4;
 constexpr int nlev            = PLEV;
 constexpr int nlevp           = nlev + 1;
 constexpr int timelevels      = 3;
+
+#endif // HOMMEXX_CUDA_BUILD
 
 } // namespace Homme
 
