7111e1165e7623290f72c15f99062bad6909446d
Luca Bertagna <lbertag@sandia.gov>
2018-01-08 17:46:29 -0700

    Added interface for BuffersManager to mpi_cxx_f90_interface
    Also, changed calls to get_connectivity, to take into account that it now
    returns a shared pointer

--- components/homme/src/share/cxx/mpi/mpi_cxx_f90_interface.cpp
+++ components/homme/src/share/cxx/mpi/mpi_cxx_f90_interface.cpp
@@ -10,10 +10,18 @@ namespace Homme
 extern "C"
 {
 
+void setup_buffer_manager (const int& num_2d_fields, const int& num_3d_fields)
+{
+  BuffersManager& buffers_manager = *Context::singleton().get_buffers_manager();
+  buffers_manager.request_num_fields (num_2d_fields, num_3d_fields);
+  buffers_manager.allocate_buffers(Context::singleton().get_connectivity());
+}
+
 void init_connectivity (const int& num_local_elems)
 {
-  Connectivity& connectivity = Context::singleton().get_connectivity();
+  Connectivity& connectivity = *Context::singleton().get_connectivity();
   connectivity.set_num_elements(num_local_elems);
+  connectivity.set_comm(Context::singleton().get_comm());
 }
 
 void add_connection (const int& first_elem_lid,  const int& first_elem_gid,  const int& first_elem_pos,  const int& first_elem_pid,
@@ -29,14 +37,14 @@ void add_connection (const int& first_elem_lid,  const int& first_elem_gid,  con
     std::abort();
   }
 
-  Connectivity& connectivity = Context::singleton().get_connectivity();
+  Connectivity& connectivity = *Context::singleton().get_connectivity();
   connectivity.add_connection(first_elem_lid-1, first_elem_gid-1, first_elem_pos-1, first_elem_pid-1,
                               second_elem_lid-1,second_elem_gid-1,second_elem_pos-1,second_elem_pid-1);
 }
 
 void finalize_connectivity ()
 {
-  Connectivity& connectivity = Context::singleton().get_connectivity();
+  Connectivity& connectivity = *Context::singleton().get_connectivity();
 
   connectivity.finalize();
 }
