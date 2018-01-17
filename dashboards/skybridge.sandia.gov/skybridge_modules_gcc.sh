7d224e884e15d6e8b33eb672c732c2aa3d14f657
Irina K. Tezaur <ikalash@sandia.gov>
2018-01-17 13:57:47 -0800

    Updating intel compiler module on Skybridge.

--- dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
+++ dashboards/skybridge.sandia.gov/skybridge_modules_gcc.sh
@@ -1,5 +1,5 @@
 source /projects/sems/modulefiles/utils/sems-modules-init.sh
-module load intel/16.0
+module load intel/18.0
 module load openmpi-intel/1.10
 module load sems-env 
 module add sems-hdf5/1.8.12/base sems-netcdf/4.4.1/exo
