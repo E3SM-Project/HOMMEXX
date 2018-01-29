f75167b3605292f1b1b45e4ac9990e4077b3bfd7
Andrew M. Bradley <ambradl@sandia.gov>
2018-01-28 19:51:13 -0700

    Remove old code. GPU spot check shows good speedup.
    apply_rspheremp old  1 1 4.320000e+02   1.076084e+00  1.076 (0 0) 1.076 (0 0)
    apply_rspheremp new  1 1 4.320000e+02   5.100465e-02  0.051 (0 0) 0.051 (0 0)

--- components/homme/src/share/cxx/EulerStepFunctor.hpp
+++ components/homme/src/share/cxx/EulerStepFunctor.hpp
@@ -97,24 +97,6 @@ public:
                          e.m_omega_p :
                          e.m_derived_divdp_proj);
 
-    start_timer("apply_rspheremp");
-#if 0
-    MDRangePolicy<ExecSpace,5> policy5({0,0,0,0,0},
-                                       {c.num_elems, c.qsize, NP, NP, NUM_LEV},
-                                       {1,1,1,1,1});
-    Kokkos::Experimental::md_parallel_for(
-      policy5,KOKKOS_LAMBDA(int ie, int q, int igp, int jgp, int ilev) {
-        e.m_qdp(ie,c.np1_qdp,q,igp,jgp,ilev) *= e.m_rspheremp(ie,igp,jgp);
-      });
-
-    MDRangePolicy<ExecSpace,4> policy4({0,0,0,0},
-                                       {c.num_elems, NP, NP, NUM_LEV},
-                                       {1,1,1,1});
-    Kokkos::Experimental::md_parallel_for(
-      policy4,KOKKOS_LAMBDA(int ie, int igp, int jgp, int ilev) {
-        f_dss(ie,igp,jgp,ilev) *= e.m_rspheremp(ie,igp,jgp);
-      });
-#else
     Kokkos::parallel_for(
       Kokkos::RangePolicy<ExecSpace>(0, c.num_elems*c.qsize*NP*NP*NUM_LEV),
       KOKKOS_LAMBDA(const int it) {
@@ -135,9 +117,7 @@ public:
         const int ilev = it % NUM_LEV;
         f_dss(ie,igp,jgp,ilev) *= e.m_rspheremp(ie,igp,jgp);
       });
-#endif
     ExecSpace::fence();
-    stop_timer("apply_rspheremp");
   }
 
 private:
