a39bb437db7ac8ac5ccc8372dd14bc893ac913e9
Luca Bertagna <lbertag@sandia.gov>
2018-01-30 12:12:20 -0700

    Added error codes to the Errors namespace

--- src/share/cxx/mpi/ErrorDefs.cpp
+++ src/share/cxx/mpi/ErrorDefs.cpp
@@ -9,10 +9,17 @@
 namespace Homme {
 namespace Errors {
 
-void runtime_abort(std::string message, int code) {
+void runtime_check(bool cond, const std::string& message, int code) {
+  if (!cond) {
+    runtime_abort(message,code);
+  }
+}
+
+void runtime_abort(const std::string& message, int code) {
   std::cerr << message << std::endl << "Exiting..." << std::endl;
   finalize_hommexx_session();
   MPI_Abort(MPI_COMM_WORLD, code);
 }
-}
-}
+
+} // namespace Homme
+} // namespace Errors
--- src/share/cxx/mpi/ErrorDefs.hpp
+++ src/share/cxx/mpi/ErrorDefs.hpp
@@ -1,14 +1,19 @@
+#ifndef HOMMEXX_ERRORDEFS_HPP
+#define HOMMEXX_ERRORDEFS_HPP
 
-#ifndef _ERRORDEFS_HPP_
-#define _ERRORDEFS_HPP_
+#include <string>
 
 namespace Homme {
 namespace Errors {
 
-void runtime_abort(std::string message, int code);
+void runtime_check(bool cond, const std::string& message, int code);
+void runtime_abort(const std::string& message, int code);
 
+static constexpr int err_unknown_option           = 11;
+static constexpr int err_unsupported_option       = 12;
 static constexpr int err_negative_layer_thickness = 101;
-}
-}
 
-#endif // _ERRORDEFS_HPP_
+} // namespace Homme
+} // namespace Errors
+
+#endif // HOMMEXX_ERRORDEFS_HPP
