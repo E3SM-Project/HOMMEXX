e7272335033810288d9e10dcee4709fb940df415
Andrew M. Bradley <ambradl@sandia.gov>
2018-02-01 16:58:29 -0700

    Clean up debug code.

--- src/share/cxx/mpi/Hommexx_Debug.hpp
+++ src/share/cxx/mpi/Hommexx_Debug.hpp
@@ -14,7 +14,7 @@
 //       of the error (so that one knows where the problem is, even
 //       when running outside of a debugger or without a breakpoint).
 
-#define HOMMEXX_MPI_CHECK_ERROR(X)                            \
+#define HOMMEXX_MPI_CHECK_ERROR(X,comm)                       \
   {                                                           \
     int err_code = X;                                         \
     if (err_code!=MPI_SUCCESS) {                              \
@@ -23,12 +23,12 @@
       MPI_Error_string(err_code,err_str,&resultlen);          \
       printf("Hommexx mpi error: %s\n",err_str);              \
       printf("   at line %d of file %s\n",__LINE__,__FILE__); \
-      MPI_Abort(m_connectivity->get_comm().m_mpi_comm,err_code);        \
+      MPI_Abort(comm,err_code);                               \
     }                                                         \
   }
 
 #else
-#define HOMMEXX_MPI_CHECK_ERROR(X) X
+#define HOMMEXX_MPI_CHECK_ERROR(X,comm) X
 #endif
 
 #endif // HOMMEXX_DEBUG_HPP
