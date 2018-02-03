6cecad50fad0c2ae7ed127dd33cf4c354a735de2
Michael Deakin <mfdeakin@cs.unc.edu>
2018-02-02 18:54:54 -0700

    Move ErrorDefs to a more appropriate place, as it's only related to MPI by its use of MPI_Abort Also add a TRACE level of debugging, not enablable by CMake yet

---
+++ src/share/cxx/ErrorDefs.hpp
@@ -0,0 +1,48 @@
+
+#ifndef _ERRORDEFS_HPP_
+#define _ERRORDEFS_HPP_
+
+#ifndef NDEBUG
+#define DEBUG_PRINT(...) \
+  do {                   \
+    printf(__VA_ARGS__); \
+  } while(false)
+// This macro always evaluates eval, but
+// This enables us to define variables specifically for use
+// in asserts Note this can still cause issues
+#define DEBUG_EXPECT(eval, expected) \
+  {                                  \
+    auto v = eval;                   \
+    assert(v == expected);           \
+  }
+#else
+#define DEBUG_PRINT(...) \
+  do {                   \
+  } while(false)
+#define DEBUG_EXPECT(eval, expected) \
+  do {                               \
+    eval;                            \
+  } while(false)
+#endif
+
+#ifndef NTRACE
+#define TRACE_PRINT(...)
+do {
+  printf(__VA_ARGS__);
+} while(false)
+#else
+#define TRACE_PRINT(...)
+do {
+} while(false)
+#endif
+
+    namespace Homme {
+  namespace Errors {
+
+  void runtime_abort(std::string message, int code);
+
+  static constexpr int err_negative_layer_thickness = 101;
+  }
+}
+
+#endif // _ERRORDEFS_HPP_
