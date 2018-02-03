b32db69c37990d6ea87a33e0dbcc81b876bc5318
Michael Deakin <mfdeakin@cs.unc.edu>
2018-02-02 19:08:43 -0700

    Fix formatting

--- components/homme/src/share/cxx/ErrorDefs.hpp
+++ components/homme/src/share/cxx/ErrorDefs.hpp
@@ -11,38 +11,38 @@
 // This enables us to define variables specifically for use
 // in asserts Note this can still cause issues
 #define DEBUG_EXPECT(eval, expected) \
-  {                                  \
-    auto v = eval;                   \
-    assert(v == expected);           \
-  }
+do {                                 \
+  auto v = eval;                     \
+  assert(v == expected);             \
+} while(false)
 #else
 #define DEBUG_PRINT(...) \
-  do {                   \
-  } while(false)
+do {                     \
+} while(false)
 #define DEBUG_EXPECT(eval, expected) \
-  do {                               \
-    eval;                            \
-  } while(false)
+do {                                 \
+  eval;                              \
+} while(false)
 #endif
 
 #ifdef DEBUG_TRACE
-#define TRACE_PRINT(...)
-do {
-  printf(__VA_ARGS__);
+#define TRACE_PRINT(...) \
+do {                     \
+  printf(__VA_ARGS__);   \
 } while(false)
 #else
-#define TRACE_PRINT(...)
-do {
+#define TRACE_PRINT(...) \
+do {                     \
 } while(false)
 #endif
 
-    namespace Homme {
-  namespace Errors {
+namespace Homme {
+namespace Errors {
 
-  void runtime_abort(std::string message, int code);
+void runtime_abort(std::string message, int code);
 
-  static constexpr int err_negative_layer_thickness = 101;
-  }
+static constexpr int err_negative_layer_thickness = 101;
+}  // namespace Errors
 }
 
-#endif // _ERRORDEFS_HPP_
+#endif  // _ERRORDEFS_HPP_
