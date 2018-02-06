788439b198e21507186e80f267d765eb34bf0ae8
Luca Bertagna <lbertag@sandia.gov>
2018-02-06 09:48:23 -0700

    Fixing compilation error due to a sloppy merge with master.
    My bad.

--- components/homme/src/share/cxx/ErrorDefs.hpp
+++ components/homme/src/share/cxx/ErrorDefs.hpp
@@ -44,7 +44,7 @@ void runtime_check(bool cond, const std::string& message, int code);
 void runtime_abort(const std::string& message, int code);
 
 static constexpr int err_unknown_option           = 11;
-static constexpr int err_unsupported_option       = 12;
+static constexpr int err_not_implemented          = 12;
 static constexpr int err_negative_layer_thickness = 101;
 
 } // namespace Errors
