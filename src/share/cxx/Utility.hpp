3fa6590556082fbea00dfc73f2eacde4eb042007
Luca Bertagna <lbertag@sandia.gov>
2018-02-07 17:10:21 -0700

    Added variant of sync_to_host for tracers view without time levels

--- src/share/cxx/Utility.hpp
+++ src/share/cxx/Utility.hpp
@@ -411,6 +411,34 @@ sync_to_host(Source_T source, Dest_T dest) {
 template <typename Source_T, typename Dest_T>
 typename std::enable_if<
     exec_view_mappable<Source_T,
+                       Scalar * [QSIZE_D][NP][NP][NUM_LEV]>::value
+  &&host_view_mappable<Dest_T,
+                       Real * [QSIZE_D][NUM_PHYSICAL_LEV][NP][NP]>::value,
+    void>::type
+sync_to_host(Source_T source, Dest_T dest) {
+  typename Source_T::HostMirror source_mirror(
+      Kokkos::create_mirror_view(source));
+  Kokkos::deep_copy(source_mirror, source);
+  for (int ie = 0; ie < source.extent_int(0); ++ie) {
+    for (int tracer = 0; tracer < QSIZE_D; ++tracer) {
+      for (int vector_level = 0, level = 0; vector_level < NUM_LEV;
+           ++vector_level) {
+        for (int vector = 0; vector < VECTOR_SIZE; ++vector, ++level) {
+          for (int igp = 0; igp < NP; ++igp) {
+            for (int jgp = 0; jgp < NP; ++jgp) {
+              dest(ie, tracer, level, igp, jgp) =
+                  source_mirror(ie, tracer, igp, jgp, vector_level)[vector];
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+template <typename Source_T, typename Dest_T>
+typename std::enable_if<
+    exec_view_mappable<Source_T,
                        Scalar * [Q_NUM_TIME_LEVELS][QSIZE_D][NP][NP][NUM_LEV]>::
         value &&host_view_mappable<
             Dest_T, Real * [Q_NUM_TIME_LEVELS][QSIZE_D][NUM_PHYSICAL_LEV][NP]
