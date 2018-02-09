ce3bcb2f7357a277493b540e7ac64b8dee3f7410
Andrew M. Bradley <ambradl@sandia.gov>
2018-02-08 17:59:43 -0700

    Fix warning.

--- src/share/cxx/vertical_remap.cpp
+++ src/share/cxx/vertical_remap.cpp
@@ -12,7 +12,6 @@ namespace Homme
 void vertical_remap(const Real dt)
 {
   // Get control and simulation params
-  Control& data = Context::singleton().get_control();
   VerticalRemapManager& vrm = Context::singleton().get_vertical_remap_manager();
   TimeLevel& tl = Context::singleton().get_time_level();
   vrm.run_remap(tl.np1, tl.np1_qdp, dt);
