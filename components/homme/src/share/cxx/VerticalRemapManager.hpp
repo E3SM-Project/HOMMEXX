82ace65ec32ae59cf83310dc2cfdf951f233184e
Andrew M. Bradley <ambradl@sandia.gov>
2018-02-08 17:45:04 -0700

    Impl once-constructed RemapFunctor.

--- components/homme/src/share/cxx/VerticalRemapManager.hpp
+++ components/homme/src/share/cxx/VerticalRemapManager.hpp
@@ -8,7 +8,7 @@ namespace Homme {
 struct VerticalRemapManager {
   VerticalRemapManager();
 
-  void run_remap() const;
+  void run_remap(int np1, int n0_qdp, double dt) const;
 
 private:
   struct Impl;
