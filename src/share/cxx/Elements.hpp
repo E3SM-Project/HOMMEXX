36f43056a4fa13facb8b343b73671ad7f2e537c8
Luca Bertagna <lbertag@sandia.gov>
2018-02-22 10:32:28 -0700

    More changes towards Elements as view of structs.
    1) Pulled the Element class outside of Elements, since it is used often
       inside functors, and Elements::Element is just too long...
    2) Added a getter for the whole m_elements view. This is needed when dealing
       with outer ||for lambda's in functors. Perhaps not 100% necessary, may
       go away soon.
    3) Some minor fixes/improvements to the init/push/pull routines.

--- src/share/cxx/Elements.hpp
+++ src/share/cxx/Elements.hpp
@@ -5,70 +5,71 @@
 
 namespace Homme {
 
+// A struct holding data in a single 3d column (one 2d elem, all vertical levels)
+struct Element {
+  static size_t size() {
+    size_t sizes_sum =
+          6*NP*NP                                      // six 2d scalar views
+      + 2*6*NP*NP                                      // two 2d tensor views
+      +   8*NP*NP*NUM_LEV*VECTOR_SIZE                  // eight 3d scalar views
+      + 2*1*NP*NP*NUM_LEV*VECTOR_SIZE                  // one 3d vector view
+      +   2*NUM_TIME_LEVELS*NP*NP*NUM_LEV*VECTOR_SIZE  // two time-dep 3d scalar views
+      + 2*1*NUM_TIME_LEVELS*NP*NP*NUM_LEV*VECTOR_SIZE  // one time-dep 3d vector view
+      +   1*NUM_TIME_LEVELS*NP*NP;                     // one time-dep 2d scalar view
+
+    return sizes_sum;
+  }
+
+  void init (Real* buffer);
+
+  // Coriolis term
+  ExecViewUnmanaged<Real [NP][NP]> m_fcor;
+  // Quadrature weights and metric tensor
+  ExecViewUnmanaged<Real [NP][NP]>        m_mp;
+  ExecViewUnmanaged<Real [NP][NP]>        m_spheremp;
+  ExecViewUnmanaged<Real [NP][NP]>        m_rspheremp;
+  ExecViewUnmanaged<Real [2][2][NP][NP]>  m_metinv;
+  ExecViewUnmanaged<Real [NP][NP]>        m_metdet;
+  // Prescribed surface geopotential height at eta = 1
+  ExecViewUnmanaged<Real [NP][NP]> m_phis;
+
+  // D (map for covariant coordinates) and D^{-1}
+  ExecViewUnmanaged<Real [2][2][NP][NP]> m_d;
+  ExecViewUnmanaged<Real [2][2][NP][NP]> m_dinv;
+
+  // Omega is the 'pressure vertical velocity' in papers,
+  // but omega=Dp/Dt  (not really vertical velocity).
+  // In homme omega is scaled, derived%omega_p=(1/p)*(Dp/Dt)
+  ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_omega_p;
+  // Geopotential height field
+  ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_phi;
+  // weighted velocity flux for consistency
+  ExecViewUnmanaged<Scalar [2][NP][NP][NUM_LEV]> m_derived_vn0;
+
+  // Velocity in lon lat basis
+  ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][2][NP][NP][NUM_LEV]> m_v;
+  // Temperature
+  ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][NP][NP][NUM_LEV]> m_t;
+  // dp ( it is dp/d\eta * delta(eta)), or pseudodensity
+  ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][NP][NP][NUM_LEV]> m_dp3d;
+
+  ExecViewUnmanaged<Real [NUM_TIME_LEVELS][NP][NP]> m_ps_v;
+
+  // eta=$\eta$ is the vertical coordinate
+  // eta_dot_dpdn = $\dot{eta}\frac{dp}{d\eta}$
+  //    (note there are NUM_PHYSICAL_LEV+1 of them)
+  ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_eta_dot_dpdn;
+  ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]>
+    m_derived_dp,                // for dp_tracers at physics timestep
+    m_derived_divdp,             // divergence of dp
+    m_derived_divdp_proj,        // DSSed divdp
+    m_derived_dpdiss_biharmonic, // mean dp dissipation tendency, if nu_p>0
+    m_derived_dpdiss_ave;        // mean dp used to compute psdiss_tens
+};
+
 /* Per element data - specific velocity, temperature, pressure, etc. */
 class Elements {
 public:
-
-  struct Element {
-    static size_t size() {
-      size_t sizes_sum =
-            6*NP*NP                                      // six 2d scalar views
-        + 2*6*NP*NP                                      // two 2d tensor views
-        +   8*NP*NP*NUM_LEV*VECTOR_SIZE                  // eight 3d scalar views
-        + 2*1*NP*NP*NUM_LEV*VECTOR_SIZE                  // one 3d vector view
-        +   2*NUM_TIME_LEVELS*NP*NP*NUM_LEV*VECTOR_SIZE  // two time-dep 3d scalar views
-        + 2*1*NUM_TIME_LEVELS*NP*NP*NUM_LEV*VECTOR_SIZE  // one time-dep 3d vector view
-        +   1*NUM_TIME_LEVELS*NP*NP;                     // one time-dep 2d scalar view
-
-      return sizes_sum;
-    }
-
-    void init (Real* buffer);
-
-    // Coriolis term
-    ExecViewUnmanaged<Real [NP][NP]> m_fcor;
-    // Quadrature weights and metric tensor
-    ExecViewUnmanaged<Real [NP][NP]>        m_mp;
-    ExecViewUnmanaged<Real [NP][NP]>        m_spheremp;
-    ExecViewUnmanaged<Real [NP][NP]>        m_rspheremp;
-    ExecViewUnmanaged<Real [2][2][NP][NP]>  m_metinv;
-    ExecViewUnmanaged<Real [NP][NP]>        m_metdet;
-    // Prescribed surface geopotential height at eta = 1
-    ExecViewUnmanaged<Real [NP][NP]> m_phis;
-
-    // D (map for covariant coordinates) and D^{-1}
-    ExecViewUnmanaged<Real [2][2][NP][NP]> m_d;
-    ExecViewUnmanaged<Real [2][2][NP][NP]> m_dinv;
-
-    // Omega is the 'pressure vertical velocity' in papers,
-    // but omega=Dp/Dt  (not really vertical velocity).
-    // In homme omega is scaled, derived%omega_p=(1/p)*(Dp/Dt)
-    ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_omega_p;
-    // Geopotential height field
-    ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_phi;
-    // weighted velocity flux for consistency
-    ExecViewUnmanaged<Scalar [2][NP][NP][NUM_LEV]> m_derived_vn0;
-
-    // Velocity in lon lat basis
-    ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][2][NP][NP][NUM_LEV]> m_v;
-    // Temperature
-    ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][NP][NP][NUM_LEV]> m_t;
-    // dp ( it is dp/d\eta * delta(eta)), or pseudodensity
-    ExecViewUnmanaged<Scalar [NUM_TIME_LEVELS][NP][NP][NUM_LEV]> m_dp3d;
-
-    ExecViewUnmanaged<Real [NUM_TIME_LEVELS][NP][NP]> m_ps_v;
-
-    // eta=$\eta$ is the vertical coordinate
-    // eta_dot_dpdn = $\dot{eta}\frac{dp}{d\eta}$
-    //    (note there are NUM_PHYSICAL_LEV+1 of them)
-    ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]> m_eta_dot_dpdn;
-    ExecViewUnmanaged<Scalar [NP][NP][NUM_LEV]>
-      m_derived_dp,                // for dp_tracers at physics timestep
-      m_derived_divdp,             // divergence of dp
-      m_derived_divdp_proj,        // DSSed divdp
-      m_derived_dpdiss_biharmonic, // mean dp dissipation tendency, if nu_p>0
-      m_derived_dpdiss_ave;        // mean dp used to compute psdiss_tens
-  };
 
   //buffer views are temporaries that matter only during local RK steps
   //(dynamics and tracers time step).
@@ -122,7 +123,11 @@ public:
   Elements() = default;
 
   KOKKOS_INLINE_FUNCTION
-  const Element& get_element(const int ie) {
+  ExecViewUnmanaged<Element*> get_elements() const {
+    return m_elements;
+  }
+  KOKKOS_INLINE_FUNCTION
+  const Element& get_element(const int ie) const {
     return m_elements(ie);
   }
 
