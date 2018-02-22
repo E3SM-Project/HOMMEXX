93bdf354ea1239aaf2b83de7dc527c9c623eba9c
Michael Deakin <mdeakin@sandia.gov>
2018-02-21 18:42:05 -0700

    Fix qlim and qtens usage, remove them completely from Elements

--- src/share/cxx/Tracers.hpp
+++ src/share/cxx/Tracers.hpp
@@ -29,6 +29,11 @@ public:
     return m_tracers(ie, iq);
   }
 
+  KOKKOS_INLINE_FUNCTION
+  int num_tracers() const {
+    return m_tracers.extent_int(1);
+  }
+
   ExecViewManaged<Scalar * [QSIZE_D][NP][NP][NUM_LEV]> m_q;
   ExecViewManaged<Scalar * [Q_NUM_TIME_LEVELS][QSIZE_D][NP][NP][NUM_LEV]> m_qdp;
   ExecViewManaged<Scalar * [QSIZE_D][NP][NP][NUM_LEV]> qtens_biharmonic;
