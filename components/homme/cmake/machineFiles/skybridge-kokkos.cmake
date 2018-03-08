86b59faeb7faf5a77b614d74b5fdccaed939adfc
Oksana Guba <oksana.guba@gmail.com>
2018-03-08 09:32:39 -0700

    minor tweaks for builds and tests

--- components/homme/cmake/machineFiles/skybridge-kokkos.cmake
+++ components/homme/cmake/machineFiles/skybridge-kokkos.cmake
@@ -1,33 +1,32 @@
-# 
-# CMake initial cache file for Sandia's redsky
-# configurd for redsky's netcdf-intel/4.1 module 
-#
+#OG my profile on skybridge:
+#module purge
+#source /projects/sems/modulefiles/utils/sems-modules-init.sh
+#module load sems-intel/17.0.0
+#module load sems-openmpi/1.10.5
+#module load sems-python/2.7.9
+#module load sems-cmake/3.5.2
+#module load sems-git
+#module load sems-hdf5/1.8.12/base
+#module load sems-netcdf/4.4.1/exo
+####for homme
+#export NETCDF_PATH=${SEMS_NETCDF_ROOT}
+#export PATH=${NETCDF_PATH}/bin/:${PATH}
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$NETCDF_PATH/lib
+
 SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
 SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
 SET (CMAKE_CXX_COMPILER mpicc CACHE FILEPATH "")
 
-# Openmpi 1.8 only
-#SET (USE_MPI_OPTIONS "--map-by node:SPAN" CACHE FILEPATH "")
-
-# Openmpi 1.6
-#SET (USE_MPI_OPTIONS "-loadbalance" CACHE FILEPATH "")
-
-# this is ignored if we use FORCE_Fortran_FLAGS
-SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
-
-# override cmake's intel defaults:
-# default cmake options for Intel: 
-#      -assume byterecl -fp-model precise -ftz -g -O3 
-# -O3 causes problems on redsky when openMP is enabled (even for 1 thread)
-#
-#SET (FORCE_Fortran_FLAGS "-openmp -fp-model fast -ftz -g -O2" CACHE STRING "")
 SET (FORCE_Fortran_FLAGS "-openmp -traceback -fp-model precise -ftz -g -O2" CACHE STRING "")
 
-SET (NETCDF_DIR /projects/ccsm/tpl/netcdf/4.3.2/intel/13.0.1/openmpi/1.6.5 CACHE FILEPATH "")
-SET (HDF5_DIR /projects/ccsm/tpl/hdf5/1.8.11/intel/13.0.1/openmpi/1.6.5 CACHE FILEPATH "")
+SET (WITH_PNETCDF FALSE CACHE FILEPATH "")
+SET (NETCDF_DIR $ENV{SEMS_NETCDF_ROOT} CACHE FILEPATH "")
+SET (PNETCDF_DIR $ENV{SEMS_NETCDF_ROOT} CACHE FILEPATH "")
+SET (HDF5_DIR $ENV{SEMS_HDF5_ROOT} CACHE FILEPATH "")
+SET (ZLIB_DIR $ENV{SEMS_ZLIB_ROOT} CACHE FILEPATH "")
 
 SET(USE_TRILINOS OFF CACHE BOOL "")
-SET(KOKKOS_PATH "/ascldap/users/onguba/kokkos/build/install/" CACHE STRING "")
+SET(KOKKOS_PATH "/ascldap/users/onguba/kokkos/build-single-nodebug/install/" CACHE STRING "")
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
