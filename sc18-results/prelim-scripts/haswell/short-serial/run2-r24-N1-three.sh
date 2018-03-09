c38cb410be4556aad8bf3af1b0dd89b4c135b67a
Oksana Guba <oksana.guba@gmail.com>
2018-03-09 09:43:57 -0800

    saving prelim scripts on nersc (but not saving prelim results) and first results from cori knl

---
+++ sc18-results/prelim-scripts/haswell/short-serial/run2-r24-N1-three.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+source ~/load-haswell
+
+exech=./bld/src/preqx/preqx
+execxx=../bxx-serial/test_execs/prtcB_flat_c/prtcB_flat_c
+execxx2=../bxx-serial-old/test_execs/prtcB_flat_c/prtcB_flat_c
+nlh=homme-ne2-v1.nl
+nlxx=xx-ne2-v1.nl
+
+PER_NODE=24
+NN=1
+NMPI=$((PER_NODE*NN))
+comm="srun -N ${NN} -n ${NMPI} -c 2 --cpu_bind=cores "
+dd=`date +"%s"`
+
+for i in 1 2 ; do
+
+$comm $exech < $nlh > tmp.txt
+tail -n 5 tmp.txt
+mv HommeTime_stats time-homme-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+$comm $execxx < $nlxx > tmp.txt
+tail -n 5 tmp.txt
+mv HommeTime_stats time-xx-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+
+$comm $execxx2 < $nlxx > tmp.txt
+tail -n 5 tmp.txt
+mv HommeTime_stats time-xxOLD-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+
+done
+
+#now putput
+for i in 1 2 ; do
+
+#repetition
+n1=time-xx-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+n1OLD=time-xxOLD-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+n2=time-homme-ne2-N${NN}-r${PER_NODE}-iter${i}-${dd}
+
+echo "times from ${n1}" 
+grep "tl-sc vertical\|prim_main\|_RK2\|U3\-5\|vis_dp" $n1
+
+echo "times from ${n1OLD}" 
+grep "tl-sc vertical\|prim_main\|_RK2\|U3\-5\|vis_dp" $n1OLD
+
+echo "times from ${n2}"
+grep "vertical_remap\"\|prim_main\|_rk2\|U3\-5\|vis_dp" $n2
+
+done
+
+
+
+
---
+++ sc18-results/prelim-scripts/knl/short-serial/gettimes.sh
@@ -0,0 +1,4 @@
+grep "tl-sc vertical\|prim_main\|_RK2\|U3\-5\|vis_dp" $1
+grep "vertical_remap\|prim_main\|_rk2\|U3\-5\|vis_dp" $2
+
+
