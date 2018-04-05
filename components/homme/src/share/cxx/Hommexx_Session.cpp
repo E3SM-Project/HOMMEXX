64d688427584fb463a260590efb5170b4d6d816c
Andrew M. Bradley <ambradl@sandia.gov>
2018-04-04 18:57:04 -0600

    Fix #271 by printing HOMMEXX SHA1 at startup.

--- components/homme/src/share/cxx/Hommexx_Session.cpp
+++ components/homme/src/share/cxx/Hommexx_Session.cpp
@@ -3,6 +3,8 @@
 #include "ExecSpaceDefs.hpp"
 #include "profiling.hpp"
 #include "Context.hpp"
+#include "Hommexx_config.h"
+#include "mpi/Comm.hpp"
 
 #include <iostream>
 
@@ -21,6 +23,8 @@ void initialize_hommexx_session ()
   ExecSpace::print_configuration(std::cout, true);
 
   // Put here other initialization routines (e.g., MPI)
+  if (Context::singleton().get_comm().m_rank == 0)
+    std::cout << "HOMMEXX SHA1: " << HOMMEXX_SHA1 << "\n";
 }
 
 void finalize_hommexx_session ()
