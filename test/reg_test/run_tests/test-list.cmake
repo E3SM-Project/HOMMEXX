cfb9ccfe42a53214c1bf79037821d8dc1e7cc6c5
Oksana Guba <oksana.guba@gmail.com>
2018-04-30 15:17:44 -0600

    adding a tensor hv test

--- test/reg_test/run_tests/test-list.cmake
+++ test/reg_test/run_tests/test-list.cmake
@@ -10,6 +10,8 @@ SET(HOMME_TESTS
   prtcB-r3-dry-c
   prtcB-r3-q6-dry-f
   prtcB-r3-q6-dry-c
+  prtcB-r3-tensorhv-dry-f
+  prtcB-r3-tensorhv-dry-c
   prtcA-r0-moist-f
   prtcA-r3-moist-f
   prtcA-r0-moist-c
@@ -28,6 +30,7 @@ SET (COMPARE_F_C_TEST
   prtcB-r0-dry
   prtcB-r3-dry
   prtcB-r3-q6-dry
+  prtcB-r3-tensorhv-dry
   prtcA-r0-moist
   prtcA-r3-moist
   prtcB-r0-moist
