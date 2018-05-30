04b29783778399c33514091a1c3dd8a8681929d0
Michael Deakin <mdeakin@sandia.gov>
2018-05-30 14:15:02 -0600

    Add the 3 Clause BSD copyright

--- src/share/cxx/prim_advec_tracers_remap.cpp
+++ src/share/cxx/prim_advec_tracers_remap.cpp
@@ -1,3 +1,9 @@
+/********************************************************************************
+ * HOMMEXX 1.0: Copyright of Sandia Corporation
+ * This software is released under the BSD license
+ * See the file 'COPYRIGHT' in the HOMMEXX/src/share/cxx directory
+ *******************************************************************************/
+
 #include "Context.hpp"
 #include "EulerStepFunctor.hpp"
 #include "SimulationParams.hpp"
@@ -84,4 +90,3 @@ void prim_advec_tracers_remap_RK2 (const Real dt)
 }
 
 } // namespace Homme
-
