e7350275e2cfb7b18fcb80e0e2774721a9ac5a25
Michael Deakin <mdeakin@sandia.gov>
2018-07-24 12:43:57 -0600

    First implementation of the tracer forcing test

--- test_execs/share_ut/cam_forcing_ut.cpp
+++ test_execs/share_ut/cam_forcing_ut.cpp
@@ -65,7 +65,7 @@ void state_forcing(
     const ExecViewUnmanaged<Scalar * [NUM_TIME_LEVELS][NP][NP][NUM_LEV]> &t,
     const ExecViewUnmanaged<Scalar * [NUM_TIME_LEVELS][2][NP][NP][NUM_LEV]> &v);
 
-TEST_CASE("cam_forcing", "states") {
+TEST_CASE("cam_forcing_states", "cam_forcing") {
   constexpr const int num_elems = 4;
   const Real dt_q = 1.0;
   const int np1 = 1;
@@ -123,6 +123,96 @@ TEST_CASE("cam_forcing", "states") {
                   v_f90(ie, np1, k, 0, igp, jgp));
           REQUIRE(v_mirror(ie, np1, 1, igp, jgp, ilev)[vlev] ==
                   v_f90(ie, np1, k, 1, igp, jgp));
+        }
+      }
+    }
+  }
+}
+
+TEST_CASE("cam_forcing_tracers", "cam_forcing") {
+  constexpr int num_elems = 2;
+  constexpr int num_q = QSIZE_D;
+  constexpr MoistDry moisture = MoistDry::MOIST;
+
+  const Real dt_q = 1.0;
+  const int np1 = 1;
+  const int np1_qdp = 2;
+
+  std::random_device rd;
+  rngAlg engine(rd());
+  std::uniform_real_distribution<Real> dist(0.125, 1000.0);
+
+  HostViewManaged<Real * [QSIZE_D][NUM_PHYSICAL_LEV][NP][NP]> fq_f90(
+      "Tracer Forcing F90", num_elems);
+  ExecViewManaged<Scalar * [QSIZE_D][NP][NP][NUM_LEV]> fq_cxx(
+      "Tracer Forcing cxx", num_elems);
+  genRandArray(fq_f90, engine, dist);
+  sync_to_device(fq_f90, fq_cxx);
+
+  HostViewManaged<Real * [NUM_TIME_LEVELS][NP][NP]> ps_v_f90(
+      "Pressure Coord F90", num_elems);
+  ExecViewManaged<Real * [NUM_TIME_LEVELS][NP][NP]> ps_v_cxx(
+      "Pressure Coord cxx", num_elems);
+  genRandArray(ps_v_f90, engine, dist);
+  Kokkos::deep_copy(ps_v_cxx, ps_v_f90);
+
+  HostViewManaged<Real * [Q_NUM_TIME_LEVELS][QSIZE_D][NUM_PHYSICAL_LEV][NP][NP]>
+  qdp_f90("Tracer Ratio F90", num_elems);
+  ExecViewManaged<Scalar * [Q_NUM_TIME_LEVELS][QSIZE_D][NP][NP][NUM_LEV]>
+  qdp_cxx("Tracer Ratio cxx", num_elems);
+  genRandArray(qdp_f90, engine, dist);
+  sync_to_device(qdp_f90, qdp_cxx);
+
+  HostViewManaged<Real * [QSIZE_D][NUM_PHYSICAL_LEV][NP][NP]> q_f90(
+      "Tracer Mass F90", num_elems);
+  ExecViewManaged<Scalar * [QSIZE_D][NP][NP][NUM_LEV]> q_cxx("Tracer Mass cxx",
+                                                             num_elems);
+  genRandArray(q_f90, engine, dist);
+  sync_to_device(q_f90, q_cxx);
+
+  HybridVCoord hvcoord;
+  hvcoord.random_init(rd());
+  auto hybrid_ai_f90 = Kokkos::create_mirror_view(hvcoord.hybrid_ai);
+  Kokkos::deep_copy(hybrid_ai_f90, hvcoord.hybrid_ai);
+  auto hybrid_bi_f90 = Kokkos::create_mirror_view(hvcoord.hybrid_bi);
+  Kokkos::deep_copy(hybrid_bi_f90, hvcoord.hybrid_bi);
+
+  TimeLevel tl;
+  tl.np1 = np1 - 1;
+  tl.np1_qdp = np1_qdp - 1;
+
+  tracer_forcing(fq_cxx, hvcoord, tl, num_q, moisture, dt_q, ps_v_cxx, qdp_cxx,
+                 q_cxx);
+  auto qdp_mirror = Kokkos::create_mirror_view(qdp_cxx);
+  Kokkos::deep_copy(qdp_mirror, qdp_cxx);
+  auto q_mirror = Kokkos::create_mirror_view(q_cxx);
+  Kokkos::deep_copy(q_mirror, q_cxx);
+
+  for (int ie = 0; ie < num_elems; ++ie) {
+    // void cam_forcing_tracers(const double &dt_q, const double &ps0, const int
+    // &np1,
+    //                          const int &np1_qdp, const bool &wet,
+    //                          const double *hyai, const double *hybi,
+    //                          const double *FQ, double *Qdp, double *ps_v);
+
+    cam_forcing_tracers(dt_q, hvcoord.ps0, np1, np1_qdp,
+                        moisture == MoistDry::MOIST, hybrid_ai_f90.data(),
+                        hybrid_bi_f90.data(), Homme::subview(fq_f90, ie).data(),
+                        Homme::subview(qdp_f90, ie).data(),
+                        Homme::subview(ps_v_f90, ie).data());
+    for (int q_idx = 0; q_idx < QSIZE_D; ++q_idx) {
+      for (int k = 0; k < NUM_PHYSICAL_LEV; ++k) {
+        const int ilev = k / VECTOR_SIZE;
+        const int vlev = k % VECTOR_SIZE;
+        for (int igp = 0; igp < NP; ++igp) {
+          for (int jgp = 0; jgp < NP; ++jgp) {
+            for (int tl_idx = 0; tl_idx < Q_NUM_TIME_LEVELS; ++tl_idx) {
+              REQUIRE(qdp_f90(ie, tl_idx, q_idx, k, igp, jgp) ==
+                      qdp_mirror(ie, tl_idx, q_idx, igp, jgp, ilev)[vlev]);
+            }
+            REQUIRE(q_f90(ie, q_idx, k, igp, jgp) ==
+                    q_mirror(ie, q_idx, igp, jgp, ilev)[vlev]);
+          }
         }
       }
     }
