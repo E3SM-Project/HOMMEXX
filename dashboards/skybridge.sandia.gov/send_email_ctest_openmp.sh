502b8bef2e2aba8f7dd83fb7b4884f6311c69048
Irina K. Tezaur <ikalash@sandia.gov>
2018-09-08 11:37:31 -0600

    Taking Michael's email off list of nightly testing recipients.

--- dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
+++ dashboards/skybridge.sandia.gov/send_email_ctest_openmp.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybrid
 TTTTT=`grep "(Timeout)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
 TT=`grep "...   Passed" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXopenmp.txt -c`
 
-/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /projects/hommexx/nightlyHOMMEXXCDash/results_hommexx_openmp
+/bin/mail -s "HOMMEXX (master, OpenMP KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /projects/hommexx/nightlyHOMMEXXCDash/results_hommexx_openmp
 
--- dashboards/skybridge.sandia.gov/send_email_ctest_serial.sh
+++ dashboards/skybridge.sandia.gov/send_email_ctest_serial.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybrid
 TTTTT=`grep "(Timeout)" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXserial.txt -c`
 TT=`grep "...   Passed" /projects/hommexx/nightlyHOMMEXXCDash/nightly_log_skybridgeHOMMEXXserial.txt -c`
 
-/bin/mail -s "HOMMEXX (master, Serial KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, mdeakin@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /projects/hommexx/nightlyHOMMEXXCDash/results_hommexx_serial
+/bin/mail -s "HOMMEXX (master, Serial KokkosNode): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, lbertag@sandia.gov, dsunder@sandia.gov, onguba@sandia.gov, ambradl@sandia.gov" < /projects/hommexx/nightlyHOMMEXXCDash/results_hommexx_serial
 
